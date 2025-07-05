0260859224e0ff2414f6892edf47d0aa80c2c0c6
Wxw <weiwei_2024024@qq.com>
2025-07-05 19:43:39 +0800

    modify

--- weekly-problems/src/class_2022_8_4/Code01_MaxXFromStock.java
+++ weekly-problems/src/class_2022_8_4/Code01_MaxXFromStock.java
@@ -7,7 +7,7 @@ package class_2022_8_4;
 // 第3天，价格是10
 // 第9天，价格是30
 // 那么第3天和第9天的指标X = 10 + 30 - (9 - 3) = 34
-// 返回arr中最大的指标X
+// 返回arr中最大的指标X.
 // 时间复杂度O(N)
 public class Code01_MaxXFromStock {
     public static int maxX(int[] arr) {
