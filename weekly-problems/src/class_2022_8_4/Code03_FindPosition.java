69bdf8ece14d52732fca80d6da45bbcea1c2dee1
Wxw <weiwei_2024024@qq.com>
2025-07-06 10:27:56 +0800

    modify

--- weekly-problems/src/class_2022_8_4/Code03_FindPosition.java
+++ weekly-problems/src/class_2022_8_4/Code03_FindPosition.java
@@ -13,7 +13,14 @@ package class_2022_8_4;
 // 接下来输入n个整数，代表ai，某道题的分值
 // 输出最大期望分值，精确到小数点后2位
 // 数据 1m<=n<=50000
-// 简单题, 将每道题做错失掉的分数的概率排个序，拿到前m个作为答案
+// 简单题,：将每道题做错失掉的分数的概率排个序，拿到前m个作为答案
+
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+
 // 题目2
 // 小团在地图上放了3个定位装置，想依赖他们进行定位！
 // 地图是一个n*n的棋盘，
@@ -26,4 +33,79 @@ package class_2022_8_4;
 // 输出最小字典序的信标位置。
 // 1 <= 所有数据值 <= 50000
 public class Code03_FindPosition {
+    public static int[] find(int n, int[] a, int[] b, int[] c, int ad, int bd, int cd) {
+        int[] x1 = null;
+        int r1 = Integer.MAX_VALUE;
+        int[] x2 = null;
+        int r2 = 0;
+        int[] x3 = null;
+        int r3 = 0;
+        if (ad < r1) {
+            x1 = a;
+            r1 = ad;
+            x2 = b;
+            r2 = bd;
+            x3 = c;
+            r3 = cd;
+        }
+        if (bd < r1) {
+            x1 = b;
+            r1 = bd;
+            x2 = a;
+            r2 = ad;
+            x3 = c;
+            r3 = cd;
+        }
+        if (cd < r1) {
+            x1 = c;
+            r1 = cd;
+            x2 = a;
+            r2 = ad;
+            x3 = b;
+            r3 = bd;
+        }
+        //下面开始进行BFS
+        int[] cur = {x1[0] - r1, x1[1]}; //初始化搜索起始点
+        Queue<int[]> queue = new LinkedList<>();//记录BFS节点存储
+        HashSet<String> visited = new HashSet<>();//记录已访问过的目标节点
+        ArrayList<int[]> ans = new ArrayList<>();//记录所有候选点
+        queue.add(cur);
+        visited.add(cur[0] + "_" + cur[1]);//将初始点标记为已访问
+
+        while (!queue.isEmpty()) {
+            cur = queue.poll();
+            if (cur[0] >= 1 && cur[0] <= n && cur[1] >= 1 && cur[1] <= n && distance(cur[0], cur[1], x2) == r2 && distance(cur[0], cur[1], x3) == r3) {
+                ans.add(cur);
+            }
+            if (ans.size() == 2) {
+                break;
+            }
+            add(cur[0] - 1, cur[1] - 1, x1, r1, queue, visited);
+            add(cur[0] - 1, cur[1], x1, r1, queue, visited);
+            add(cur[0] - 1, cur[1] + 1, x1, r1, queue, visited);
+            add(cur[0], cur[1] - 1, x1, r1, queue, visited);
+            add(cur[0], cur[1] + 1, x1, r1, queue, visited);
+            add(cur[0] + 1, cur[1] - 1, x1, r1, queue, visited);
+            add(cur[0] + 1, cur[1], x1, r1, queue, visited);
+            add(cur[0] + 1, cur[1] + 1, x1, r1, queue, visited);
+        }
+        //将字典序小的返回
+        if (ans.size() == 1 || ans.get(0)[0] < ans.get(1)[0] || (ans.get(0)[0] == ans.get(1)[0] && ans.get(0)[1] < ans.get(1)[1])) {
+            return ans.get(0);
+        } else {
+            return ans.get(1);
+        }
+    }
+
+    public static void add(int x, int y, int[] c, int r, Queue<int[]> queue, HashSet<String> visited) {
+        String key = x + "_" + y;
+        if (distance(x, y, c) == r && !visited.contains(key)) {
+            queue.add(new int[]{x, y});
+            visited.add(key);
+        }
+    }
+
+    public static int distance(int x, int y, int[] c) {
+        return Math.abs(x - c[0]) + Math.abs(y - c[1]);
+    }
 }
