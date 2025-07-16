bb07d5d19221f1b971242a2ae97a11d710041a74
Wxw <weiwei_2024024@qq.com>
2025-07-16 14:27:13 +0800

    modify

---
+++ coding-for-great-offer/src/Class19/Code01_LRUCache.java
@@ -0,0 +1,5 @@
+package Class19;
+//LRU内存/缓存替换算法
+//Leetcode题目：https://leetcode.com/problems/lru-cache/
+public class Code01_LRUCache {
+}
---
+++ coding-for-great-offer/src/Class19/Code02_LFUCache.java
@@ -0,0 +1,5 @@
+package Class19;
+//LFU内存/缓存替换算法
+//Leetcode题目：https://leetcode.com/problems/lfu-cache/
+public class Code02_LFUCache {
+}
