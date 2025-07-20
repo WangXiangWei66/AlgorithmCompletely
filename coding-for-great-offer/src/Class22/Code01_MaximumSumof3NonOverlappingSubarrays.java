e6538255a6eef6d999b4c4112097c5796aad6281
Wxw <weiwei_2024024@qq.com>
2025-07-20 13:31:24 +0800

    modify

---
+++ coding-for-great-offer/src/Class22/Code01_MaximumSumof3NonOverlappingSubarrays.java
@@ -0,0 +1,5 @@
+package Class22;
+//给定数组nums由正整数组成，找到三个互不重叠的子数组的最大和。每个子数组的长度为k，我们要使这3*k个项的和最大化。返回每个区间起始索引的列表（索引从 0 开始）。如果有多个结果，返回字典序最小的一个。
+//Leetcode题目：https://leetcode.com/problems/maximum-sum-of-3-non-overlapping-subarrays/
+public class Code01_MaximumSumof3NonOverlappingSubarrays {
+}
---
+++ coding-for-great-offer/src/Class22/Code03_TrappingRainWaterII.java
@@ -0,0 +1,5 @@
+package Class22;
+//给你一个 m x n 的矩阵，其中的值均为非负整数，代表二维高度图每个单元的高度，请计算图中形状最多能接多少体积的雨水。
+//Leetcode题目：https://leetcode.com/problems/trapping-rain-water-ii/
+public class Code03_TrappingRainWaterII {
+}
