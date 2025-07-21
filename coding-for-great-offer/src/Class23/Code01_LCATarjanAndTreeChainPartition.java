81491e18de74452570902a58d3e731f552153d09
Wxw <weiwei_2024024@qq.com>
2025-07-21 12:14:45 +0800

    modify

---
+++ coding-for-great-offer/src/Class23/Code01_LCATarjanAndTreeChainPartition.java
@@ -0,0 +1,10 @@
+package Class23;
+//给定数组father大小为N，表示一共有N个节点
+//father[i] = j 表示点i的父亲是点j， father表示的树一定是一棵树而不是森林
+//queries是二维数组，大小为M*2，每一个长度为2的数组都表示一条查询
+//[4,9], 表示想查询4和9之间的最低公共祖先…
+//[3,7], 表示想查询3和7之间的最低公共祖先…
+//tree和queries里面的所有值，都一定在0~N-1之间
+//返回一个数组ans，大小为M，ans[i]表示第i条查询的答案
+public class Code01_LCATarjanAndTreeChainPartition {
+}
