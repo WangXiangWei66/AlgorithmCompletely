c6c1cc4d24aed957bcd7bdfffaf5e5718e12622c
Wxw <weiwei_2024024@qq.com>
2025-08-26 10:36:02 +0800

    modify

--- coding-for-great-offer/src/Class43/Code01_SumNoPositiveMinCost.java
+++ coding-for-great-offer/src/Class43/Code01_SumNoPositiveMinCost.java
@@ -1,4 +1,7 @@
 package Class43;
+
+import java.util.Arrays;
+
 //来自微软面试
 //给定一个正数数组arr长度为n、正数x、正数y
 //你的目标是让arr整体的累加和<=0
@@ -9,4 +12,171 @@ package Class43;
 //返回你达到目标的最小代价
 //数据规模 : 面试时面试官没有说数据规模
 public class Code01_SumNoPositiveMinCost {
+
+    public static int minOpStep1(int[] arr, int x, int y) {
+        int sum = 0;
+        for (int num : arr) {
+            sum += num;
+        }
+        return process1(arr, x, y, 0, sum);
+    }
+
+    public static int process1(int[] arr, int x, int y, int i, int sum) {
+        if (sum <= 0) {
+            return 0;
+        }
+        if (i == arr.length) {
+            return Integer.MAX_VALUE;
+        }
+        //对当前元素不做任何选择
+        int p1 = process1(arr, x, y, i + 1, sum);
+        //将答盎前元素变为0，代价为X
+        int p2 = Integer.MAX_VALUE;
+        int next2 = process1(arr, x, y, i + 1, sum - arr[i]);
+        if (next2 != Integer.MAX_VALUE) {
+            p2 = x + next2;
+        }
+        //将当前元素变为-num，代价为y
+        int p3 = Integer.MAX_VALUE;
+        int next3 = process1(arr, x, y, i + 1, sum - (arr[i] << 1));//累加和会减少2*arr[i]
+        if (next3 != Integer.MAX_VALUE) {
+            p3 = y + next3;
+        }
+        return Math.min(p1, Math.min(p2, p3));
+    }
+
+
+    public static int minOpStep2(int[] arr, int x, int y) {
+        Arrays.sort(arr);
+        int n = arr.length;
+        //反转数组
+        for (int l = 0, r = n - 1; l <= r; l++, r--) {
+            int tmp = arr[l];
+            arr[l] = arr[r];
+            arr[r] = tmp;
+        }
+        //优先取反
+        if (x >= y) {
+            int sum = 0;
+            for (int num : arr) {
+                sum += num;
+            }
+            int cost = 0;
+            for (int i = 0; i < n && sum > 0; i++) {
+                sum -= arr[i] << 1;
+                cost += y;
+            }
+            return cost;
+        } else {
+            //计算后缀和，arr[i]表示从i到n-1元素的总和
+            for (int i = n - 2; i >= 0; i--) {
+                arr[i] += arr[i + 1];
+            }
+            int benefit = 0;
+            //找到最小的left，使从left到n-1的元素和<=benefit
+            int left = mostLeft(arr, 0, benefit);
+            int cost = left * x;//初始代价,对0~left-1执行清零操作
+            for (int i = 0; i < n - 1; i++) {
+                benefit += arr[i] - arr[i + 1];
+                left = mostLeft(arr, i + 1, benefit);
+                cost = Math.min(cost, (i + 1) * y + (left - i - 1) * x);
+            }
+            return cost;
+        }
+    }
+
+    public static int mostLeft(int[] arr, int l, int v) {
+        int r = arr.length - 1;
+        int m = 0;
+        int ans = arr.length;//默认所有元素都需要处理
+        while (l <= r) {
+            m = (l + r) / 2;
+            if (arr[m] <= v) {
+                ans = m;
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+        return ans;
+    }
+
+    //使用了滑动窗口来替换二分
+    public static int minOpStep3(int[] arr, int x, int y) {
+        Arrays.sort(arr);
+        int n = arr.length;
+        for (int l = 0, r = n - 1; l <= r; l++, r--) {
+            int tmp = arr[l];
+            arr[l] = arr[r];
+            arr[r] = tmp;
+        }
+        if (x >= y) {
+            int sum = 0;
+            for (int num : arr) {
+                sum += num;
+            }
+            int cost = 0;
+            for (int i = 0; i < n && sum > 0; i++) {
+                sum -= arr[i] << 1;
+                cost += y;
+            }
+            return cost;
+        } else {
+            int benefit = 0;//记录取反操作带来的收益
+            int cost = arr.length * x;
+            int holdSum = 0;//当前不需要处理的元素
+            //yRight取反操作的右边界
+            //holdLeft：需要保留元素的左边界
+            for (int yRight = 0, holdLeft = n; yRight < holdLeft - 1; yRight++) {
+                benefit += arr[yRight];
+                //移动holdLeft指针，找到最大范围的可保留元素（这些元素的总和<=取反带来的收益）
+                while (holdLeft - 1 > yRight && holdSum + arr[holdLeft - 1] <= benefit) {
+                    holdSum += arr[holdLeft - 1];
+                    holdLeft--;
+                }
+                cost = Math.min(cost, (yRight + 1) * y + (holdLeft - yRight - 1) * x);
+            }
+            return cost;
+        }
+    }
+
+    public static int[] randomArray(int len, int v) {
+        int[] arr = new int[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = (int) (Math.random() * v) + 1;
+        }
+        return arr;
+    }
+
+    public static int[] copyArray(int[] arr) {
+        int[] ans = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            ans[i] = arr[i];
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int n = 12;
+        int v = 20;
+        int c = 10;
+        int testTime = 10000;
+        System.out.println("test begin!");
+        for (int i = 0; i < testTime; i++) {
+            int len = (int) (Math.random() * n);
+            int[] arr = randomArray(len, v);
+            int[] arr1 = copyArray(arr);
+            int[] arr2 = copyArray(arr);
+            int[] arr3 = copyArray(arr);
+            int x = (int) (Math.random() * c);
+            int y = (int) (Math.random() * c);
+            int ans1 = minOpStep1(arr1, x, y);
+            int ans2 = minOpStep2(arr2, x, y);
+            int ans3 = minOpStep3(arr3, x, y);
+            if (ans1 != ans2 || ans1 != ans3) {
+                System.out.println("Oops");
+            }
+        }
+        System.out.println("test end!");
+    }
 }
--- coding-for-great-offer/src/Class43/Code02_MinCostToYeahArray.java
+++ coding-for-great-offer/src/Class43/Code02_MinCostToYeahArray.java
@@ -1,4 +1,7 @@
 package Class43;
+
+import java.util.Arrays;
+
 //来自360笔试
 //给定一个正数数组arr，长度为n，下标0~n-1
 //arr中的0、n-1位置不需要达标，它们分别是最左、最右的位置
@@ -8,4 +11,301 @@ package Class43;
 //返回至少要多少步可以让arr变成yeah！数组
 //数据规模 : 数组长度 <= 10000，数组中的值<=500
 public class Code02_MinCostToYeahArray {
+
+    public static final int INVALID = Integer.MAX_VALUE;
+
+    public static int minCost0(int[] arr) {
+        if (arr == null || arr.length < 3) {
+            return 0;
+        }
+        int n = arr.length;
+        //寻找数组中元素的最小值
+        int min = INVALID;
+        for (int num : arr) {
+            min = Math.min(min, num);
+        }
+        int base = min - n;//元素可以较少到的最低值
+        return process0(arr, base, 0);
+    }
+
+    public static int process0(int[] arr, int base, int index) {
+        if (index == arr.length) {
+            for (int i = 1; i < arr.length - 1; i++) {
+                if (arr[i - 1] <= arr[i] && arr[i] >= arr[i + 1]) {
+                    return INVALID;
+                }
+            }
+            return 0;
+        } else {
+            int ans = INVALID;
+            int tmp = arr[index];//保存当前元素的原始值
+            //将当前元素减少到不同值
+            for (int cost = 0; arr[index] >= base; cost++, arr[index]--) {
+                int next = process0(arr, base, index + 1);
+                if (next != INVALID) {
+                    ans = Math.min(ans, cost + next);
+                }
+            }
+            arr[index] = tmp;
+            return ans;
+        }
+    }
+
+    public static int minCost1(int[] arr) {
+        if (arr == null || arr.length < 3) {
+            return 0;
+        }
+        int min = INVALID;
+        for (int num : arr) {
+            min = Math.min(min, num);
+        }
+        //转换数组值，对数组进行了一次偏移处理
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] += arr.length - min;
+        }
+        //从索引1开始处理，前一个元素是arr[0],初始状态preOk为true
+        return process1(arr, 1, arr[0], true);
+    }
+
+    //preOk:前一个位置的元素是否达标
+    public static int process1(int[] arr, int index, int pre, boolean preOk) {
+        if (index == arr.length - 1) {
+            return preOk || pre < arr[index] ? 0 : INVALID;
+        }
+        int ans = INVALID;
+        //前一个位置的元素已经达标了
+        if (preOk) {
+            //当前元素可以任意减小直到为0
+            for (int cur = arr[index]; cur >= 0; cur--) {
+                int next = process1(arr, index + 1, cur, cur < pre);
+                if (next != INVALID) {
+                    ans = Math.min(ans, arr[index] - cur + next);
+                }
+            }
+        } else {
+            for (int cur = arr[index]; cur > pre; cur--) {
+                int next = process1(arr, index + 1, cur, false);
+                if (next != INVALID) {
+                    ans = Math.min(ans, arr[index] - cur + next);
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static int minCost2(int[] arr) {
+        if (arr == null || arr.length < 3) {
+            return 0;
+        }
+        int min = INVALID;
+        for (int num : arr) {
+            min = Math.min(min, num);
+        }
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            arr[i] += n - min;
+        }
+        //dp[index][state][pre]:
+        //index:当前处理的元素索引
+        //state:状态标志
+        //pre；前一个元素的最终值
+        int[][][] dp = new int[n][2][];
+        for (int i = 1; i < n; i++) {
+            //第二维的元素是0或1，第三位为前一个元素的可能值范围
+            dp[i][0] = new int[arr[i - 1] + 1];
+            dp[i][1] = new int[arr[i - 1] + 1];
+            //将达标与不达标的数组，都设置为无效值
+            Arrays.fill(dp[i][0], INVALID);//初始化为无效值
+            Arrays.fill(dp[i][1], INVALID);
+        }
+        for (int pre = 0; pre <= arr[n - 2]; pre++) {
+            dp[n - 1][0][pre] = pre < arr[n - 1] ? 0 : INVALID;
+            dp[n - 1][1][pre] = 0;
+        }
+        //DP表从后往前填
+        for (int index = n - 2; index >= 1; index--) {
+            for (int pre = 0; pre <= arr[index - 1]; pre++) {
+                //处理当前元素未达标的情况
+                for (int cur = arr[index]; cur > pre; cur--) {
+                    int next = dp[index + 1][0][cur];
+                    if (next != INVALID) {
+                        dp[index][0][pre] = Math.min(dp[index][0][pre], arr[index] - cur + next);
+                    }
+                }
+                for (int cur = arr[index]; cur >= 0; cur--) {
+                    int next = dp[index + 1][cur < pre ? 1 : 0][cur];
+                    if (next != INVALID) {
+                        dp[index][1][pre] = Math.min(dp[index][1][pre], arr[index] - cur + next);
+                    }
+                }
+            }
+        }
+        return dp[1][1][arr[0]];
+    }
+
+    public static int minCost3(int[] arr) {
+        if (arr == null || arr.length < 3) {
+            return 0;
+        }
+        int min = INVALID;
+        for (int num : arr) {
+            min = Math.min(min, num);
+        }
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            arr[i] += n - min;
+        }
+        //[]:前一个元素的最终值
+        int[][][] dp = new int[n][2][];
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = new int[arr[i - 1] + 1];
+            dp[i][1] = new int[arr[i - 1] + 1];
+        }
+        //填充倒数第二个元素
+        for (int p = 0; p <= arr[n - 2]; p++) {
+            dp[n - 1][0][p] = p < arr[n - 1] ? 0 : INVALID;
+        }
+        //best：预处理问题的最优解，避免主循环中重复计算
+        int[][] best = best(dp, n - 1, arr[n - 2]);
+        for (int i = n - 2; i >= 1; i--) {
+            //p:前一个元素i-1的最终值
+            for (int p = 0; p <= arr[i - 1]; p++) {
+                if (arr[i] < p) {
+                    dp[i][1][p] = best[1][arr[i]];
+                } else {
+                    dp[i][1][p] = Math.min(best[0][p], p > 0 ? best[1][p - 1] : INVALID);
+                }
+                dp[i][0][p] = arr[i] <= p ? INVALID : best[0][p + 1];
+            }
+            //更新best数组，为上一层计算做准备
+            best = best(dp, i, arr[i - 1]);
+        }
+        return dp[1][1][arr[0]];
+    }
+
+    //best[0][p]:0状态下的从p开始的最小代价
+    //best[1][p]:1状态下的从p开始的最小代价
+    public static int[][] best(int[][][] dp, int i, int v) {
+        int[][] best = new int[2][v + 1];//存储前一个元素两种状态的最优解
+        //从最大值往最小值遍历
+        best[0][v] = dp[i][0][v];
+        for (int p = v - 1; p >= 0; p--) {
+            best[0][p] = dp[i][0][p] == INVALID ? INVALID : v - p + dp[i][0][p];
+            best[0][p] = Math.min(best[0][p], best[0][p + 1]);
+        }
+        //从最小值往最大值遍历
+        best[1][0] = dp[i][1][0] == INVALID ? INVALID : v + dp[i][1][0];
+        for (int p = 1; p <= v; p++) {
+            best[1][p] = dp[i][1][p] == INVALID ? INVALID : v - p + dp[i][1][p];
+            best[1][p] = Math.min(best[1][p], best[1][p - 1]);
+        }
+        return best;
+    }
+
+    //贪心的最优解，时间复杂度为O(n)
+    public static int year(int[] arr) {
+        if (arr == null || arr.length < 3) {
+            return 0;
+        }
+        int n = arr.length;
+        int[] nums = new int[n + 2];//扩展数组，在首尾各添加一个极大值
+        nums[0] = Integer.MAX_VALUE;
+        nums[n + 1] = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            nums[i + 1] = arr[i];//将原始的数组移动到中间的位置
+        }
+        int[] leftCost = new int[n + 2];//leftCost[i]:使1~i位置严格递减的最小代价
+        int pre = nums[0];//前一个元素的最终值，初始化为左侧极大值
+        int change = 0;
+        for (int i = 1; i <= n; i++) {
+            // 当前元素的最大可能值 = min(前一个元素值-1, 原始值)
+            // 确保严格递减（当前元素 < 前一个元素），且不超过原始值（只能减小）
+            change = Math.min(pre - 1, nums[i]);
+            leftCost[i] = nums[i] - change + leftCost[i - 1];
+            pre = change;
+        }
+        int[] rightCost = new int[n + 2];//rightCost[i]:表示i~n位置严格递减的最小代价
+        pre = nums[n + 1];
+        for (int i = n; i >= 1; i--) {
+            // 当前元素的最大可能值 = min(后一个元素值-1, 原始值)
+            // 确保严格递减（当前元素 < 后一个元素），即从右往左看是严格递增
+            change = Math.min(pre - 1, nums[i]);
+            rightCost[i] = nums[i] - change + rightCost[i + 1];
+            pre = change;
+        }
+        int ans = Integer.MAX_VALUE;
+        for (int i = 1; i <= n; i++) {
+            ans = Math.min(ans, leftCost[i] + rightCost[i + 1]);
+        }
+        return ans;
+    }
+
+    public static int[] randomArray(int len, int v) {
+        int[] arr = new int[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = (int) (Math.random() * v) + 1;
+        }
+        return arr;
+    }
+
+    public static int[] copyArray(int[] arr) {
+        int[] ans = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            ans[i] = arr[i];
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int len = 7;
+        int v = 10;
+        int testTime = 100;
+        System.out.println("=============");
+        System.out.println("功能测试开始！");
+        for (int i = 0; i < testTime; i++) {
+            int n = (int) (Math.random() * len) + 1;
+            int[] arr = randomArray(n, v);
+            int[] arr0 = copyArray(arr);
+            int[] arr1 = copyArray(arr);
+            int[] arr2 = copyArray(arr);
+            int[] arr3 = copyArray(arr);
+            int[] arr4 = copyArray(arr);
+            int ans0 = minCost0(arr0);
+            int ans1 = minCost1(arr1);
+            int ans2 = minCost2(arr2);
+            int ans3 = minCost3(arr3);
+            int ans4 = year(arr4);
+            if (ans0 != ans1 || ans0 != ans2 || ans0 != ans3 || ans0 != ans4) {
+                System.out.println("Oops");
+                break;
+            }
+        }
+        System.out.println("功能测试结束！");
+        System.out.println("==========");
+
+        System.out.println("性能测试开始！");
+        len = 10000;
+        v = 500;
+        System.out.println("生成随机数组长度：" + len);
+        System.out.println("申城随机数组值的范围：[1," + v + "]");
+        int[] arr = randomArray(len, v);
+        int[] arr3 = copyArray(arr);
+        int[] arrYear = copyArray(arr);
+        long start;
+        long end;
+        start = System.currentTimeMillis();
+        int ans3 = minCost3(arr3);
+        end = System.currentTimeMillis();
+        System.out.println("minCost3方法：");
+        System.out.println("运送结果：" + ans3 + ",时间（毫秒）：" + (end - start));
+
+        start = System.currentTimeMillis();
+        int ansYear = year(arrYear);
+        end = System.currentTimeMillis();
+        System.out.println("year方法：");
+        System.out.println("运送结果：" + ans3 + ",时间（毫秒）：" + (end - start));
+
+        System.out.println("性能测试结束！");
+        System.out.println("=============");
+    }
 }
