226da0fb705258204b2b0c1534d6a7262b2e33ee
Wxw <weiwei_2024024@qq.com>
2025-09-10 14:55:41 +0800

    modify

--- weekly-problems/src/Class_2021_12_2/Code01_FindAllPeopleWithSecret.java
+++ weekly-problems/src/Class_2021_12_2/Code01_FindAllPeopleWithSecret.java
@@ -1,13 +1,105 @@
 package Class_2021_12_2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 //给你一个整数 n ，表示有 n 个专家从 0 到 n - 1 编号。
 //另外给一个下标从 0 开始的二维整数数组 meetings ，
 //其中 meetings[i] = [xi, yi, timei] 表示专家 xi 和专家 yi 在时间 timei 要开一场会。
 //一个专家可以同时参加 多场会议 。最后，给你一个整数 firstPerson 。
-//专家 0 有一个 秘密 ，最初，他在时间 0 将这个秘密分享给了专家 firstPerson 。
+//专家 0 有一个 秘密 ，最初，他在时间0 将这个秘密分享给了专家 firstPerson 。
 //这个秘密会在每次有知晓这个秘密的专家参加会议时进行传播。更正式的表达是，每次会议，
 //如果专家 xi 在时间 timei 时知晓这个秘密，那么他将会与专家 yi 分享这个秘密，反之亦然。
 //秘密共享是 瞬时发生 的。也就是说，在同一时间，一个专家不光可以接收到秘密，还能在其他会议上与其他专家分享。
 //在所有会议都结束之后，返回所有知晓这个秘密的专家列表。你可以按 任何顺序 返回答案。
 //Leetcode链接 : https://leetcode.com/problems/find-all-people-with-secret/
 public class Code01_FindAllPeopleWithSecret {
+
+    public List<Integer> findAllPeople(int n, int[][] meetings, int firstPerson) {
+        UnionFind uf = new UnionFind(n, firstPerson);
+        int m = meetings.length;
+        Arrays.sort(meetings, (a, b) -> (a[2] - b[2]));
+        int[] help = new int[m << 1];//同一时间的会议参与者
+        help[0] = meetings[0][0];
+        help[1] = meetings[0][1];
+        int size = 2;//辅助数组中有效元素的数量
+        //将同一时间的会议参与者放入help数组
+        for (int i = 1; i < m; i++) {
+            if (meetings[i][2] != meetings[i - 1][2]) {
+                share(help, size, uf);//处理help数组中的会议
+                help[0] = meetings[i][0];
+                help[1] = meetings[i][1];
+                size = 2;
+            } else {
+                help[size++] = meetings[i][0];
+                help[size++] = meetings[i][1];
+            }
+        }
+        share(help, size, uf);
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (uf.know(i)) {
+                ans.add(i);
+            }
+        }
+        return ans;
+    }
+
+    public static void share(int[] help, int size, UnionFind uf) {
+        for (int i = 0; i < size; i += 2) {
+            uf.union(help[i], help[i + 1]);
+        }
+        for (int i = 0; i < size; i++) {
+            if (!uf.know(help[i])) {
+                //将不知道秘密的专家隔离
+                uf.isolate(help[i]);
+            }
+        }
+    }
+
+    public static class UnionFind {
+        public int[] father;
+        public boolean[] sect;//每个集合是否知晓秘密
+        public int[] help;
+
+        public UnionFind(int n, int first) {
+            father = new int[n];
+            sect = new boolean[n];
+            help = new int[n];
+            for (int i = 1; i < n; i++) {
+                father[i] = i;
+            }
+            father[first] = 0;
+            sect[0] = true;
+        }
+
+        private int find(int i) {
+            int hi = 0;
+            while (i != father[i]) {
+                help[hi++] = i;
+                i = father[i];
+            }
+            for (hi--; hi >= 0; hi--) {
+                father[help[hi]] = i;
+            }
+            return i;
+        }
+
+        public void union(int i, int j) {
+            int fatheri = find(i);
+            int fatherj = find(j);
+            if (fatheri != fatherj) {
+                sect[fatheri] |= sect[fatherj];
+            }
+        }
+        //专家是否知晓秘密
+        public boolean know(int i) {
+            return sect[find(i)];
+        }
+        //使专家成为独立的集合
+        public void isolate(int i) {
+            father[i] = i;
+        }
+    }
 }
--- weekly-problems/src/Class_2021_12_2/Code03_MagicSum.java
+++ weekly-problems/src/Class_2021_12_2/Code03_MagicSum.java
@@ -1,4 +1,7 @@
 package Class_2021_12_2;
+
+import java.util.Arrays;
+
 //arr数组长度为n, magic数组长度为m
 //比如 arr = { 3, 1, 4, 5, 7 }，如果完全不改变arr中的值，
 //那么收益就是累加和 = 3 + 1 + 4 + 5 + 7 = 20
@@ -8,4 +11,263 @@ package Class_2021_12_2;
 //返回arr尽可能大的累加和
 //n <= 10^7     m <= 10^6    arr中的值和c的范围 <= 10^12
 public class Code03_MagicSum {
+
+    public static class SegmentTree2 {
+        private int[] max;
+        private int[] change;//记录区间更新的目标值
+        private boolean[] update;
+
+        public SegmentTree2(int size) {
+            int N = size + 1;
+            max = new int[N << 2];
+            change = new int[N << 2];
+            update = new boolean[N << 2];
+        }
+
+        private void pushUp(int rt) {
+            max[rt] = Math.max(max[rt << 1], max[rt << 1 | 1]);
+        }
+
+        private void pushDown(int rt, int ln, int rn) {
+            if (update[rt]) {
+                update[rt << 1] = true;
+                update[rt << 1 | 1] = true;
+                change[rt << 1] = change[rt];
+                change[rt << 1 | 1] = change[rt];
+                max[rt << 1] = change[rt];
+                max[rt << 1 | 1] = change[rt];
+                update[rt] = false;
+            }
+        }
+
+        public void update(int L, int R, int C, int l, int r, int rt) {
+            if (L <= l && r <= R) {
+                update[rt] = true;
+                change[rt] = C;
+                max[rt] = C;
+                return;
+            }
+            int mid = (l + r) >> 1;
+            pushDown(rt, mid - l + 1, r - mid);
+            if (L <= mid) {
+                update(L, R, C, l, mid, rt << 1);
+            }
+            if (R > mid) {
+                update(L, R, C, mid + 1, r, rt << 1 | 1);
+            }
+            pushUp(rt);
+        }
+
+        public int query(int L, int R, int l, int r, int rt) {
+            if (L <= l && r <= R) {
+                return max[rt];
+            }
+            int mid = (l + r) >> 1;
+            pushDown(rt, mid - l + 1, r - mid);
+            int left = 0;
+            int right = 0;
+            if (L <= mid) {
+                left = query(L, R, l, mid, rt << 1);
+            }
+            if (R > mid) {
+                right = query(L, R, mid + 1, r, rt << 1 | 1);
+            }
+            return Math.max(left, right);
+        }
+    }
+
+    public static int maxSum1(int[] arr, int[][] magics) {
+        int[] help = Arrays.copyOf(arr, arr.length);
+        for (int[] m : magics) {
+            int l = m[0];
+            int r = m[1];
+            int c = m[2];
+            for (int i = l; i <= r; i++) {
+                help[i] = Math.max(help[i], c);
+            }
+        }
+        int sum = 0;
+        for (int num : help) {
+            sum += num;
+        }
+        return sum;
+    }
+
+    public static int maxSum2(int[] arr, int[][] magics) {
+        int n = arr.length;
+        SegmentTree2 st = new SegmentTree2(n);
+        //按照魔法值从小到大排序
+        Arrays.sort(magics, (a, b) -> (a[2] - b[2]));
+        for (int[] magic : magics) {
+            st.update(magic[0] + 1, magic[1] + 1, magic[2], 1, n, 1);
+        }
+        int ans = 0;
+        //单点查询
+        for (int i = 0; i < n; i++) {
+            ans += Math.max(st.query(i + 1, i + 1, 1, n, 1), arr[i]);
+        }
+        return ans;
+    }
+
+    public static class SegmentTree3 {
+        private int[] max;
+        private int[] change;
+        private boolean[] update;
+
+        public SegmentTree3(int size) {
+            int N = size + 1;
+            max = new int[N << 2];
+            change = new int[N << 2];
+            update = new boolean[N << 2];
+        }
+
+        private void pushUp(int rt) {
+            max[rt] = Math.max(max[rt << 1], max[rt << 1 | 1]);
+        }
+
+        public void pushDown(int rt, int ln, int rn) {
+            if (update[rt]) {
+                update[rt << 1] = true;
+                update[rt << 1 | 1] = true;
+                change[rt << 1] = change[rt];
+                change[rt << 1 | 1] = change[rt];
+                max[rt << 1] = change[rt];
+                max[rt << 1 | 1] = change[rt];
+                update[rt] = false;
+            }
+        }
+
+        public void update(int L, int R, int C, int l, int r, int rt) {
+            if (L <= l && r <= R) {
+                update[rt] = true;
+                change[rt] = C;
+                max[rt] = C;
+                return;
+            }
+            int mid = (l + r) >> 1;
+            pushDown(rt, mid - l + 1, r - mid);
+            if (L <= mid) {
+                update(L, R, C, l, mid, rt << 1);
+            }
+            if (R > mid) {
+                update(L, R, C, mid + 1, r, rt << 1 | 1);
+            }
+            pushUp(rt);
+        }
+
+        public int index = 0;
+
+        public int[] buildSingleQuery(int n) {
+            int[] ans = new int[n + 1];
+            process(ans, 1, n, 1);
+            return ans;
+        }
+
+        private void process(int[] ans, int l, int r, int rt) {
+            if (l == r) {
+                ans[index++] = max[rt];
+            } else {
+                int mid = (l + r) >> 1;
+                pushDown(rt, mid - l + 1, r - mid);
+                process(ans, l, mid, rt << 1);
+                process(ans, mid + 1, r, rt << 1 | 1);
+            }
+        }
+    }
+
+    public static int maxSum3(int[] arr, int[][] magics) {
+        int n = arr.length;
+        SegmentTree3 st = new SegmentTree3(n);
+        Arrays.sort(magics, (a, b) -> (a[2] - b[2]));
+        for (int[] magic : magics) {
+            st.update(magic[0] + 1, magic[1] + 1, magic[2], 1, n, 1);
+        }
+        int ans = 0;
+        int[] query = st.buildSingleQuery(n);
+        for (int i = 0; i < n; i++) {
+            ans += Math.max(query[i], arr[i]);
+        }
+        return ans;
+    }
+
+    public static int[] generateRandomArray(int n, int value) {
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = (int) (Math.random() * value) + 1;
+        }
+        return arr;
+    }
+
+    public static int[][] generateRandomMagics(int n, int m, int value) {
+        int[][] magics = new int[m][3];
+        for (int[] magic : magics) {
+            int a = (int) (Math.random() * n);
+            int b = (int) (Math.random() * n);
+            int c = (int) (Math.random() * value) + 1;
+            magic[0] = Math.min(a, b);
+            magic[1] = Math.max(a, b);
+            magic[2] = c;
+        }
+        return magics;
+    }
+
+    public static void main(String[] args) {
+        int n = 30;
+        int m = 15;
+        int v = 1000;
+        int testTimes = 10000;
+        System.out.println("test begin!");
+        for (int i = 0; i < testTimes; i++) {
+            int N = (int) (Math.random() * n) + 1;
+            int M = (int) (Math.random() * m) + 1;
+            int[] arr = generateRandomArray(N, v);
+            int[][] magics = generateRandomMagics(N, M, v);
+            int ans1 = maxSum1(arr, magics);
+            int ans2 = maxSum2(arr, magics);
+            int ans3 = maxSum3(arr, magics);
+            if (ans1 != ans2 || ans1 != ans3) {
+                System.out.println(ans1);
+                System.out.println(ans2);
+                System.out.println(ans3);
+                for (int num : arr) {
+                    System.out.print(num + " ");
+                }
+                System.out.println();
+                for (int[] magic : magics) {
+                    System.out.println("[" + magic[0] + "," + magic[1] + "," + magic[2] + "]");
+                }
+                System.out.println("Oops");
+                break;
+            }
+        }
+
+        System.out.println("test end");
+
+        System.out.println("性能测试开始");
+        System.out.println("n的数据量将达到10^7");
+        System.out.println("m的数据量将达到10^6");
+        System.out.println("为了防止溢出，每个值的范围控制在10以内");
+
+        n = 10000000;
+        m = 1000000;
+        v = 10;
+
+        int[] arr = generateRandomArray(n, v);
+        int[][] magics = generateRandomMagics(n, m, v);
+
+        long start;
+        long end;
+
+        start = System.currentTimeMillis();
+        int ans2 = maxSum2(arr, magics);
+        end = System.currentTimeMillis();
+        System.out.println("方法二的结果 : " + ans2 + ", 方法二的运行时间: " + (end - start) + " 毫秒");
+
+        start = System.currentTimeMillis();
+        int ans3 = maxSum3(arr, magics);
+        end = System.currentTimeMillis();
+        System.out.println("方法三的结果 : " + ans3 + ", 方法三的运行时间: " + (end - start) + " 毫秒");
+
+        System.out.println("性能测试结束");
+    }
 }
