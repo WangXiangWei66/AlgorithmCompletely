37846ca92ce0fa7ecad0689f92e4b6e8bd60979a
Wxw <weiwei_2024024@qq.com>
2025-09-17 11:35:10 +0800

    modify

--- weekly-problems/src/class_2022_01_1/Code04_MinDistanceFromLeftUpToRightDownWalk4Directions.java
+++ weekly-problems/src/class_2022_01_1/Code04_MinDistanceFromLeftUpToRightDownWalk4Directions.java
@@ -1,7 +1,111 @@
 package class_2022_01_1;
+
+import java.util.PriorityQueue;
+
 //来自学员问题
 //给定一个二维数组，其中全是非负数
 //每一步都可以往上、下、左、右四个方向运动
 //返回从左下角走到右下角的最短距离
 public class Code04_MinDistanceFromLeftUpToRightDownWalk4Directions {
+
+    public static int bestWalk1(int[][] map) {
+        int n = map.length;
+        int m = map[0].length;
+        int step = n * m - 1;//走的最大格子数
+        return process(map, n, m, step, 0, 0, 0, 0);
+    }
+
+    //step：当前已经走的步数
+    //cost：当前的累积成本
+    public static int process(int[][] map, int n, int m, int limit, int step, int row, int col, int cost) {
+        if (row < 0 || row == n || col < 0 || col == m || step > limit) {
+            return Integer.MAX_VALUE;
+        }
+        if (row == n - 1 && col == m - 1) {
+            return cost + map[row][col];
+        }
+        cost += map[row][col];
+        int p1 = process(map, n, m, limit, step + 1, row - 1, col, cost);
+        int p2 = process(map, n, m, limit, step + 1, row + 1, col, cost);
+        int p3 = process(map, n, m, limit, step + 1, row, col - 1, cost);
+        int p4 = process(map, n, m, limit, step + 1, row, col + 1, cost);
+        return Math.min(Math.min(p1, p2), Math.min(p3, p4));
+    }
+
+    //Dijkstra算法，非负权重图或网络问题
+    public static int bestWalk2(int[][] map) {
+        int n = map.length;
+        int m = map[0].length;
+        //按照路径距离从小到大排序
+        //队列中的每个元素是一个int[]数组，结构为[总距离, 行坐标, 列坐标]
+        PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        boolean[][] poped = new boolean[n][m];
+        heap.add(new int[]{map[0][0], 0, 0});
+        int ans = 0;
+        while (!heap.isEmpty()) {
+            int[] cur = heap.poll();
+            int dis = cur[0];//当前位置到起点的总距离
+            int row = cur[1];
+            int col = cur[2];
+            if (poped[row][col]) {
+                continue;
+            }
+            poped[row][col] = true;
+            if (row == n - 1 && col == m - 1) {
+                ans = dis;
+                break;
+            }
+            add(dis, row - 1, col, n, m, map, poped, heap);
+            add(dis, row + 1, col, n, m, map, poped, heap);
+            add(dis, row, col - 1, n, m, map, poped, heap);
+            add(dis, row, col + 1, n, m, map, poped, heap);
+        }
+        return ans;
+    }
+
+    //pre：上一个位置到起点的总距离
+    public static void add(int pre, int row, int col, int n, int m, int[][] map, boolean[][] used, PriorityQueue<int[]> heap) {
+        if (row >= 0 && row < n && col >= 0 && col < m && !used[row][col]) {
+            heap.add(new int[]{pre + map[row][col], row, col});
+        }
+    }
+
+    public static int[][] randomMatrix(int n, int m, int v) {
+        int[][] ans = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                ans[i][j] = (int) (Math.random() * v);
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        int m = 5;
+        int v = 10;
+        int testTimes = 10;
+        System.out.println("功能测试开始！");
+        for (int i = 0; i < testTimes; i++) {
+            int[][] map = randomMatrix(n, m, v);
+            int ans1 = bestWalk1(map);
+            int ans2 = bestWalk2(map);
+            if (ans1 != ans2) {
+                break;
+            }
+        }
+        System.out.println("功能测试结束！");
+        n = 1000;
+        m = 1000;
+        v = 100;
+        int[][] map = randomMatrix(n, m, v);
+        System.out.println("性能测试开始");
+        System.out.println("数据规模 : " + n + " * " + m);
+        System.out.println("值的范围 : 0 ~ " + v);
+        long start = System.currentTimeMillis();
+        bestWalk2(map);
+        long end = System.currentTimeMillis();
+        System.out.println("运行时间(毫秒) : " + (end - start));
+        System.out.println("性能测试结束");
+    }
 }
--- weekly-problems/src/class_2022_01_1/Problem_0913_CatAndMouse.java
+++ weekly-problems/src/class_2022_01_1/Problem_0913_CatAndMouse.java
@@ -1,4 +1,7 @@
 package class_2022_01_1;
+
+import java.util.ArrayList;
+
 //在一张 无向 图上，节点编号0~N-1。老鼠开始在1节点，猫在2节点，0号节点是洞，老鼠想进洞
 //老鼠第先出发，猫后出发，轮流行动。
 //在每个玩家的行动中，他们 必须 沿着图中与所在当前位置连通的一条边移动
@@ -10,4 +13,88 @@ package class_2022_01_1;
 //给你一张图 graph ，并假设两位玩家都都以最佳状态参与游戏，返回谁获胜
 //leetcode链接 : https://leetcode.com/problems/cat-and-mouse/
 public class Problem_0913_CatAndMouse {
+
+    //拓扑排序解法
+    public static final int MOUSE_TURN = 0, CAT_TURN = 1;//标记当前是谁的回合
+    public static final int DRAW = 0, MOUSE_WIN = 1, CAT_WIN = 2;
+
+    public int catMouseGame(int[][] graph) {
+        int n = graph.length;
+        //记录每个状态的入度
+        //[老鼠位置][猫位置][当前回合]
+        int[][][] indegree = new int[n][n][2];
+        //初始化当前回合老鼠和猫在当前位置的邻居数
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < n; j++) {
+                indegree[i][j][MOUSE_TURN] = graph[i].length;
+                indegree[i][j][CAT_TURN] = graph[j].length;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int node : graph[0]) {
+                indegree[i][node][CAT_TURN]--;//猫不能进0号洞
+            }
+        }
+        //存储每个状态的游戏结果
+        int[][][] ans = new int[n][n][2];
+        //拓扑排序处理的队列
+        int[][] queue = new int[n * n * 2][];
+        //队列的头尾指针
+        int left = 0;
+        int right = 0;
+        //两个for循环初始化已知的结果
+        for (int j = 1; j < n; j++) {
+            ans[0][j][MOUSE_TURN] = MOUSE_WIN;
+            ans[0][j][CAT_TURN] = MOUSE_WIN;
+            queue[right++] = new int[]{0, j, MOUSE_TURN};
+            queue[right++] = new int[]{0, j, CAT_TURN};
+        }
+        for (int i = 1; i < n; i++) {
+            ans[i][i][MOUSE_TURN] = CAT_WIN;
+            ans[i][i][CAT_TURN] = CAT_WIN;
+            queue[right++] = new int[]{i, i, MOUSE_TURN};
+            queue[right++] = new int[]{i, i, CAT_TURN};
+        }
+        while (left != right) {
+            int[] cur = queue[left++];
+            int mouse = cur[0], cat = cur[1], turn = cur[2];
+            int curAns = ans[mouse][cat][turn];
+            //处理当前可能到达状态的前置位置
+            for (int[] preState : preState(graph, mouse, cat, turn)) {
+                int preMouse = preState[0], preCat = preState[1], preTurn = preState[2];
+                if (ans[preMouse][preCat][preTurn] == DRAW) {
+                    boolean canWin = (curAns == MOUSE_WIN && preTurn == MOUSE_TURN) ||
+                            (curAns == CAT_WIN && preTurn == CAT_TURN);
+                    if (canWin) {
+                        ans[preMouse][preCat][preTurn] = curAns;
+                        queue[right++] = new int[]{preMouse, preCat, preTurn};
+                    } else {
+                        if (--indegree[preMouse][preCat][preTurn] == 0) {
+                            int lose = preTurn == MOUSE_TURN ? CAT_WIN : MOUSE_WIN;
+                            ans[preMouse][preCat][preTurn] = lose;
+                            queue[right++] = new int[]{preMouse, preCat, preTurn};
+                        }
+                    }
+                }
+            }
+        }
+        return ans[1][2][MOUSE_TURN];
+    }
+
+    public ArrayList<int[]> preState(int[][] graph, int mouse, int cat, int turn) {
+        ArrayList<int[]> prevStates = new ArrayList<int[]>();
+        int prevTurn = turn == MOUSE_TURN ? CAT_TURN : MOUSE_TURN;//前一回合是对方的回合
+        if (prevTurn == MOUSE_TURN) {
+            for (int prev : graph[mouse]) {
+                prevStates.add(new int[]{prev, cat, prevTurn});
+            }
+        } else {
+            for (int prev : graph[cat]) {
+                if (prev != 0) {
+                    prevStates.add(new int[]{mouse, prev, prevTurn});
+                }
+            }
+        }
+        return prevStates;
+    }
 }
