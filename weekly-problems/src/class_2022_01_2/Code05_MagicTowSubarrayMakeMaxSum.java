23d54572f656d4f0d2a43c41436635dad40c0eb5
Wxw <weiwei_2024024@qq.com>
2025-09-19 10:35:34 +0800

    modify

--- weekly-problems/src/class_2022_01_2/Code05_MagicTowSubarrayMakeMaxSum.java
+++ weekly-problems/src/class_2022_01_2/Code05_MagicTowSubarrayMakeMaxSum.java
@@ -1,4 +1,5 @@
 package class_2022_01_2;
+
 //来自美团
 //小美有一个长度为n的数组，
 //为了使得这个数组的和尽量大，她向会魔法的小团进行求助
@@ -6,4 +7,104 @@ package class_2022_01_2;
 //并将区间里的数全都变为原来的10倍
 //小团想知道他的魔法最多可以帮助小美将数组的和变大到多少?
 public class Code05_MagicTowSubarrayMakeMaxSum {
+
+    public static int maxSum1(int[] arr) {
+        int n = arr.length;
+        int[] preSum = preSum(arr);
+        //不使用魔法或只使用一个子数组的魔法所能得到的最大和
+        int ans = maxSumAtMostOneRangeMagic(preSum, 0, n - 1);
+        for (int split = 0; split < n - 1; split++) {
+            ans = Math.max(ans, maxSumAtMostOneRangeMagic(preSum, 0, split) + maxSumAtMostOneRangeMagic(preSum, split + 1, n - 1));
+        }
+        return ans;
+    }
+
+    //在[l...r]范围内，最多选择一个子数组进行10倍处理能得到的最大和
+    public static int maxSumAtMostOneRangeMagic(int[] preSum, int l, int r) {
+        if (l > r) {
+            return 0;
+        }
+        int ans = sum(preSum, l, r);//不使用魔法
+        for (int s = l; s <= r; s++) {
+            for (int e = s; e <= r; e++) {
+                ans = Math.max(ans, sum(preSum, l, s - 1) + sum(preSum, s, e) * 10 + sum(preSum, e + 1, r));
+            }
+        }
+        return ans;
+    }
+
+    public static int[] preSum(int[] arr) {
+        int n = arr.length;
+        int[] preSum = new int[n];
+        preSum[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            preSum[i] = preSum[i - 1] + arr[i];
+        }
+        return preSum;
+    }
+
+    public static int sum(int[] preSum, int l, int r) {
+        return l > r ? 0 : (l == 0 ? preSum[r] : (preSum[r] - preSum[l - 1]));
+    }
+
+    public static int maxSum2(int[] arr) {
+        int n = arr.length;
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return Math.max(arr[0], arr[0] * 10);
+        }
+        //初始化右半部分计算所需的变量
+        int sum = arr[n - 1];//从最右侧元素开始累计
+        int magic = sum * 10;//从最右侧元素开始使用魔法
+        int[] right = new int[n];//right[i]:i到n-1范围内的最大可能和
+        right[n - 1] = Math.max(sum, sum * 10);
+        for (int i = n - 2; i >= 0; i--) {
+            magic = 10 * arr[i] + Math.max(sum, magic);
+            sum += arr[i];
+            //三种情况：不使用魔法    右侧计算的最大值加上当前元素   使用魔法计算的总和
+            right[i] = Math.max(Math.max(sum, right[i + 1] + arr[i]), magic);
+        }
+        int ans = right[0];//初始时只考虑右半部分的最大值
+        sum = arr[0];
+        magic = sum * 10;
+        int dp = Math.max(sum, sum * 10);
+        ans = Math.max(ans, dp + right[1]);
+        for (int i = 1; i < n - 1; i++) {
+            magic = 10 * arr[i] + Math.max(sum, magic);
+            sum += arr[i];
+            dp = Math.max(Math.max(sum, dp + arr[i]), magic);
+            ans = Math.max(ans, dp + right[i + 1]);
+        }
+        return ans;
+    }
+
+    public static int[] randomArray(int len, int value) {
+        int[] arr = new int[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = (int) (Math.random() * value) - (int) (Math.random() * value);
+        }
+        return arr;
+    }
+
+    // 为了测试
+    public static void main(String[] args) {
+        int len = 30;
+        int value = 100;
+        int testTime = 500000;
+        System.out.println("测试开始");
+        for (int i = 0; i < testTime; i++) {
+            int n = (int) (Math.random() * len) + 1;
+            int[] arr = randomArray(n, value);
+            int ans1 = maxSum1(arr);
+            int ans2 = maxSum2(arr);
+            if (ans1 != ans2) {
+                System.out.println("出错了!");
+                break;
+            }
+        }
+        System.out.println("测试结束");
+    }
+
 }
--- weekly-problems/src/class_2022_01_2/Code06_QuietSum.java
+++ weekly-problems/src/class_2022_01_2/Code06_QuietSum.java
@@ -1,4 +1,5 @@
 package class_2022_01_2;
+
 //给定一个非负数组arr，学生依次坐在0~N-1位置，每个值表示学生的安静值
 //如果在i位置安置插班生，那么i位置的安静值变成0，同时任何同学都会被影响到而减少安静值
 //同学安静值减少的量: N - 这个同学到插班生的距离
@@ -8,6 +9,270 @@ package class_2022_01_2;
 //比如 : arr = {10,1,10,10,10}，应该返回{24,27,20,20,22}
 //arr长度 <= 10^5
 //arr中值 <= 2 * 10^5
-//注意：本题在直播课上讲错了，已经发了重新录制的视频，请一定要看更正后的视频
 public class Code06_QuietSum {
+
+    public static long[] quiet1(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return new long[0];
+        }
+        int n = arr.length;
+        long[] ans = new long[n];
+        for (int i = 0; i < n; i++) {
+            long sum = 0;
+            for (int j = 0; j < i; j++) {
+                sum += Math.max(0, arr[j] - (n - Math.abs(i - j)));
+            }
+            for (int j = i + 1; j < n; j++) {
+                sum += Math.max(0, arr[j] - (n - Math.abs(i - j)));
+            }
+            ans[i] = sum;
+        }
+        return ans;
+    }
+
+    public static long[] quiet2(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return new long[0];
+        }
+        int n = arr.length;
+        SBTree sbt = new SBTree();
+        long[] ans = new long[n];
+        //计算每个位置右侧学生的贡献
+        sbt.add(arr[n - 1] - 1);
+        for (int i = n - 2; i >= 0; i--) {
+            long moreCount = sbt.moreCount(i);
+            long moreSum = sbt.moreSum(i);
+            ans[i] = moreSum - (moreCount * i);
+            sbt.add(arr[i] + i - n);
+        }
+        //计算每个位置左侧学生的贡献
+        sbt = new SBTree();
+        sbt.add(arr[0] - 1);
+        for (int i = 1, j = n - 2; i < n; i++, j--) {
+            long moreCount = sbt.moreCount(j);
+            long moreSum = sbt.moreSum(j);
+            ans[i] += moreSum - (moreCount * j);
+            sbt.add(arr[i] + j - n);
+        }
+        return ans;
+    }
+
+    public static class SBTNode {
+        public int value;//节点存储的值
+        public int size;//以当前节点为根的子树中“不同值”的节点总数
+        public int all;//以当前节点为根的节点的总数
+        public long sum;//以当前节点为根的所有节点值的和
+        public SBTNode l;
+        public SBTNode r;
+
+        public SBTNode(int v) {
+            value = v;
+            size = 1;
+            all = 1;
+            sum = v;
+        }
+    }
+
+    public static class SBTree {
+        private SBTNode root;
+
+        //右旋
+        private SBTNode rightRotate(SBTNode cur) {
+            int curCount = cur.all - (cur.l != null ? cur.l.all : 0) - (cur.r != null ? cur.r.all : 0);//当前节点的自身计数
+            long curSum = cur.sum - (cur.l != null ? cur.l.sum : 0) - (cur.r != null ? cur.r.sum : 0);
+            SBTNode leftNode = cur.l;
+            cur.l = leftNode.r;
+            leftNode.r = cur;
+            leftNode.size = cur.size;
+            leftNode.all = cur.all;
+            leftNode.sum = cur.sum;
+            cur.size = (cur.l != null ? cur.l.size : 0) + (cur.r != null ? cur.r.size : 0) + 1;
+            cur.all = (cur.l != null ? cur.l.all : 0) + (cur.r != null ? cur.r.all : 0) + curCount;
+            cur.sum = (cur.l != null ? cur.l.sum : 0) + (cur.r != null ? cur.r.sum : 0) + curSum;
+            return leftNode;
+        }
+
+        //左旋
+        private SBTNode leftRotate(SBTNode cur) {
+            int curCount = cur.all - (cur.l != null ? cur.l.all : 0) - (cur.r != null ? cur.r.all : 0);
+            long curSum = cur.sum - (cur.l != null ? cur.l.sum : 0) - (cur.r != null ? cur.r.sum : 0);
+            SBTNode rightNode = cur.r;
+            cur.r = rightNode.l;
+            rightNode.l = cur;
+            rightNode.size = cur.size;
+            rightNode.all = cur.all;
+            rightNode.sum = cur.sum;
+            cur.size = (cur.l != null ? cur.l.size : 0) + (cur.r != null ? cur.r.size : 0) + 1;
+            cur.all = (cur.l != null ? cur.l.all : 0) + (cur.r != null ? cur.r.all : 0) + curCount;
+            cur.sum = (cur.l != null ? cur.l.sum : 0) + (cur.r != null ? cur.r.sum : 0) + curSum;
+            return rightNode;
+        }
+
+        private SBTNode maintain(SBTNode cur) {
+            if (cur == null) {
+                return null;
+            }
+            long leftSize = cur.l != null ? cur.l.size : 0;
+            long leftLeftSize = cur.l != null && cur.l.l != null ? cur.l.l.size : 0;
+            long LeftRightSize = cur.l != null && cur.l.r != null ? cur.l.r.size : 0;
+            long rightSize = cur.r != null ? cur.r.size : 0;
+            long rightLeftSize = cur.r != null && cur.r.l != null ? cur.r.l.size : 0;
+            long rightRightSize = cur.r != null && cur.r.r != null ? cur.r.r.size : 0;
+            if (leftLeftSize > rightSize) {
+                cur = rightRotate(cur);
+                cur.r = maintain(cur.r);
+                cur = maintain(cur);
+            } else if (LeftRightSize > rightSize) {
+                cur.l = leftRotate(cur.l);
+                cur = rightRotate(cur);
+                cur.l = maintain(cur.l);
+                cur.r = maintain(cur.r);
+                cur = maintain(cur);
+            } else if (rightRightSize > leftSize) {
+                cur = leftRotate(cur);
+                cur.l = maintain(cur.l);
+                cur = maintain(cur);
+            } else if (rightLeftSize > leftSize) {
+                cur.r = rightRotate(cur.r);
+                cur = leftRotate(cur);
+                cur.l = maintain(cur.l);
+                cur.r = maintain(cur.r);
+                cur = maintain(cur);
+            }
+            return cur;
+        }
+
+        //先判断是否已经包含了v，再执行插入
+        public void add(int v) {
+            root = add(root, v, contains(root, v));
+        }
+
+        private boolean contains(SBTNode cur, int v) {
+            if (cur == null) {
+                return false;
+            } else if (cur.value == v) {
+                return true;
+            } else if (cur.value > v) {
+                return contains(cur.l, v);
+            } else {
+                return contains(cur.r, v);
+            }
+        }
+
+        private SBTNode add(SBTNode cur, int v, boolean contains) {
+            if (cur == null) {
+                return new SBTNode(v);
+            } else {
+                if (!contains) {
+                    cur.size++;
+                }
+                cur.all++;
+                cur.sum += v;
+                if (cur.value == v) {
+                    return cur;
+                } else if (cur.value > v) {
+                    cur.l = add(cur.l, v, contains);
+                } else {
+                    cur.r = add(cur.r, v, contains);
+                }
+                return maintain(cur);
+            }
+        }
+
+        public long moreCount(int num) {
+            return moreCount(root, num);
+        }
+
+        private long moreCount(SBTNode cur, int num) {
+            if (cur == null) {
+                return 0;
+            }
+            if (cur.value <= num) {
+                return moreCount(cur.r, num);
+            } else {
+                return cur.all - (cur.l != null ? cur.l.all : 0) + moreCount(cur.l, num);
+            }
+        }
+
+        public long moreSum(int num) {
+            return moreSum(root, num);
+        }
+
+        private long moreSum(SBTNode cur, int num) {
+            if (cur == null) {
+                return 0;
+            }
+            if (cur.value <= num) {
+                return moreSum(cur.r, num);
+            } else {
+                return cur.sum - (cur.l != null ? cur.l.sum : 0) + moreSum(cur.l, num);
+            }
+        }
+    }
+
+    public static int[] randomArray(int len, int v) {
+        int[] ans = new int[len];
+        for (int i = 0; i < len; i++) {
+            ans[i] = (int) (Math.random() * (v + 1));
+        }
+        return ans;
+    }
+
+    public static boolean sameArray(long[] arr1, long[] arr2) {
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        int n = arr1.length;
+        for (int i = 0; i < n; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // 为了测试
+    public static void main(String[] args) {
+        int N = 1000;
+        int V = 5000;
+        int testTime = 10000;
+        System.out.println("功能测试开始");
+        for (int i = 0; i < testTime; i++) {
+            int size = (int) (Math.random() * N) + 1;
+            int[] arr = randomArray(size, V);
+            long[] ans1 = quiet1(arr);
+            long[] ans2 = quiet2(arr);
+            if (!sameArray(ans1, ans2)) {
+                System.out.println("出错了！");
+                for (int num : arr) {
+                    System.out.print(num + " ");
+                }
+                System.out.println();
+                for (long num : ans1) {
+                    System.out.print(num + " ");
+                }
+                System.out.println();
+                for (long num : ans2) {
+                    System.out.print(num + " ");
+                }
+                System.out.println();
+                break;
+            }
+        }
+        System.out.println("功能测试结束");
+
+        System.out.println("性能测试开始");
+        N = 200000;
+        V = 500000;
+        long start;
+        long end;
+        int[] arr = randomArray(N, V);
+        start = System.currentTimeMillis();
+        quiet2(arr);
+        end = System.currentTimeMillis();
+        System.out.println("测试数组长度 : " + N);
+        System.out.println("测试数组数值最大值 : " + V);
+        System.out.println("运行时间（毫秒） : " + (end - start));
+        System.out.println("性能测试结束");
+    }
 }
