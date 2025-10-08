1cc83298e82d3a3460ef6d9752030fe934aadaa1
Wxw <weiwei_2024024@qq.com>
2025-10-08 10:11:11 +0800

    modify

--- weekly-problems/src/class_2022_04_2/Code04_AllJobFinishTime.java
+++ weekly-problems/src/class_2022_04_2/Code04_AllJobFinishTime.java
@@ -1,4 +1,9 @@
 package class_2022_04_2;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 //来自美团
 //3.26笔试
 //给定一个正数n, 表示有0~n-1号任务
@@ -9,4 +14,37 @@ package class_2022_04_2;
 //返回一个长度为n的数组ans，表示每个任务完成的时间
 //输入可以保证没有循环依赖
 public class Code04_AllJobFinishTime {
+
+    public static int[] finishTime(int n, int[] time, int[][] matrix) {
+        //每个任务完成后，还能存储的后续任务
+        ArrayList<ArrayList<Integer>> nexts = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            nexts.add(new ArrayList<>());
+        }
+        //每个任务依赖的前置任务
+        int[] in = new int[n];
+        for (int[] line : matrix) {
+            nexts.get(line[1]).add(line[0]);
+            in[line[0]]++;
+        }
+        //处理入度为0的任务，即进行拓扑排序
+        Queue<Integer> zeroInQueue = new LinkedList<>();
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (in[i] == 0) {
+                zeroInQueue.add(i);
+            }
+        }
+        while (!zeroInQueue.isEmpty()) {
+            int cur = zeroInQueue.poll();
+            ans[cur] += time[cur];//入度为0的任务的完成的时间就是自身
+            for (int next : nexts.get(cur)) {
+                ans[next] = Math.max(ans[next], ans[cur]);
+                if (--in[next] == 0) {
+                    zeroInQueue.add(next);
+                }
+            }
+        }
+        return ans;
+    }
 }
--- weekly-problems/src/class_2022_04_2/Code06_PerfectPairNumber.java
+++ weekly-problems/src/class_2022_04_2/Code06_PerfectPairNumber.java
@@ -1,4 +1,13 @@
 package class_2022_04_2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.StreamTokenizer;
+import java.util.HashMap;
+
 //来自阿里
 //x = { a, b, c, d }
 //y = { e, f, g, h }
@@ -8,4 +17,42 @@ package class_2022_04_2;
 //题目给定N个小数组，每个小数组长度都是K
 //返回这N个小数组中，有多少完美对
 public class Code06_PerfectPairNumber {
+
+    public static long perfectPairs(int[][] matrix) {
+        long ans = 0;
+        //记录每个特征字符串出现的次数
+        HashMap<String, Integer> counts = new HashMap<>();
+        for (int[] arr : matrix) {
+            StringBuilder self = new StringBuilder();//构建当前数组的特征字符串
+            StringBuilder minus = new StringBuilder();//构建当前数组的互补特征字符串
+            for (int i = 1; i < arr.length; i++) {
+                self.append("_" + (arr[i] - arr[i - 1]));
+                minus.append("_" + (arr[i - 1] - arr[i]));
+            }
+            ans += counts.getOrDefault(minus.toString(), 0);
+            counts.put(self.toString(), counts.getOrDefault(self.toString(), 0) + 1);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StreamTokenizer in = new StreamTokenizer(br);
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+        while (in.nextToken() != StreamTokenizer.TT_EOF) {
+            int n = (int) in.nval;
+            in.nextToken();
+            int m = (int) in.nval;
+            int[][] matrix = new int[n][m];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    in.nextToken();
+                    matrix[i][j] = (int) in.nval;
+                }
+            }
+            long ans = perfectPairs(matrix);
+            out.println(ans);
+            out.flush();
+        }
+    }
 }
