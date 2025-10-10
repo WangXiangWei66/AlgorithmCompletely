b5e91ef141ccc345a546a321fed287a885cd9dc0
Wxw <weiwei_2024024@qq.com>
2025-10-10 10:04:33 +0800

    modify

--- weekly-problems/src/class_2022_04_3/Code05_ModKSubstringNumbers.java
+++ weekly-problems/src/class_2022_04_3/Code05_ModKSubstringNumbers.java
@@ -1,7 +1,42 @@
 package class_2022_04_3;
+
 //来自微众
 //4.11笔试
 //给定n位长的数字字符串和正数k，求该子符串能被k整除的子串个数
 //(n<=1000，k<=100)
 public class Code05_ModKSubstringNumbers {
+
+    public static int modWays1(String s, int k) {
+        int n = s.length();
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (Long.valueOf(s.substring(i, j + 1)) % k == 0) {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+
+    //任何子串都可以看作某个位置结尾的数字
+    public static int modWays2(String s, int k) {
+        int[] cur = new int[k];//当前状态下，各余数对应的子串数量
+        int[] next = new int[k];//临时存储下一轮计算状态
+        int mod = 0;//当前结尾的数字对k的余数
+        int ans = 0;//累积符合条件的子串总数
+        for (char cha : s.toCharArray()) {
+            for (int i = 0; i < k; i++) {
+                next[(i * 10) % k] += cur[i];//在上一轮的基础上，继续添加在末尾
+                cur[i] = 0;
+            }
+            int[] tmp = cur;
+            cur = next;
+            next = tmp;
+            mod = (mod * 10 + (cha - '0')) % k;
+            ans += (mod == 0 ? 1 : 0) + cur[mod];
+            cur[mod]++;
+        }
+        return ans;
+    }
 }
--- weekly-problems/src/class_2022_05_1/Code01_JumMinSameValue.java
+++ weekly-problems/src/class_2022_05_1/Code01_JumMinSameValue.java
@@ -1,12 +1,63 @@
 package class_2022_05_1;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 //来自蔚来汽车
-//给你一个整数数组 arr ，你一开始在数组的第一个元素处（下标为 0）。
-//每一步，你可以从下标 i 跳到下标 i + 1 、i - 1 或者 j ：
+//给你一个整数数组arr，你一开始在数组的第一个元素处（下标为 0）。
+//每一步，你可以从下标i跳到下标i + 1 、i - 1 或者 j ：
 //i + 1 需满足：i + 1 < arr.length
-//i - 1 需满足：i - 1 >= 0
-//j 需满足：arr[i] == arr[j] 且 i != j
-//请你返回到达数组最后一个元素的下标处所需的 最少操作次数 。
+//i - 1需满足：i - 1 >= 0
+//j需满足：arr[i] == arr[j]且i != j
+//请你返回到达数组最后一个元素的下标处所需的最少操作次数。
 //注意：任何时候你都不能跳到数组外面。
 //leetcode测试链接 : https://leetcode-cn.com/problems/jump-game-iv/
 public class Code01_JumMinSameValue {
+
+    public static int minJumps(int[] arr) {
+        int n = arr.length;
+        // 创建值到索引列表的映射，存储相同值的所有下标
+        HashMap<Integer, ArrayList<Integer>> valueIndex = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            if (!valueIndex.containsKey(arr[i])) {
+                valueIndex.put(arr[i], new ArrayList<>());
+            }
+            valueIndex.get(arr[i]).add(i);
+        }
+        boolean[] visited = new boolean[n];
+        //数组实现队列，存储待访问索引
+        int[] queue = new int[n];
+        int l = 0;
+        int r = 0;
+        queue[r++] = 0;
+        visited[0] = true;
+        int jump = 0;//记录跳跃次数
+        while (l != r) {
+            int tmp = r;
+            for (; l < tmp; l++) {
+                int cur = queue[l];
+                if (cur == n - 1) {
+                    return jump;
+                }
+                if (cur + 1 < n && !visited[cur + 1]) {
+                    visited[cur + 1] = true;
+                    queue[r++] = cur + 1;
+                }
+                if (cur > 0 && !visited[cur - 1]) {
+                    visited[cur - 1] = true;
+                    queue[r++] = cur - 1;
+                }
+                for (int next : valueIndex.get(arr[cur])) {
+                    if (!visited[next]) {
+                        visited[next] = true;
+                        queue[r++] = next;
+                    }
+                }
+                //清空当前值对应的索引
+                valueIndex.get(arr[cur]).clear();
+            }
+            jump++;
+        }
+        return -1;
+    }
 }
