7affa9d916dda8a2ffb01162266e57af701db8b7
Wxw <weiwei_2024024@qq.com>
2025-10-11 11:34:26 +0800

    modify

--- weekly-problems/src/class_2022_05_1/Code02_WhoWin21Balls.java
+++ weekly-problems/src/class_2022_05_1/Code02_WhoWin21Balls.java
@@ -6,4 +6,142 @@ package class_2022_05_1;
 //最终谁的总球数为偶数，谁赢
 //请问谁有必胜策略
 public class Code02_WhoWin21Balls {
+
+    public static String win(int balls){
+        return process(0,balls,0,0);
+    }
+
+    // turn 谁的回合！
+    // turn == 0 甲回合
+    // turn == 1 乙回合
+    // rest剩余球的数量
+    // 之前，jiaBalls、yiBalls告诉你！
+    // 当前，根据turn，知道是谁的回合！
+    // 当前，还剩多少球，rest
+    // 返回：谁会赢！
+    public static String process(int turn,int rest,int jia,int yi){
+        if (rest == 0) {
+            return (jia & 1)==0 ? "甲":"乙";
+        }
+        if (turn == 0) {
+            for(int pick = 1;pick<=Math.min(rest,3);pick++){
+                if (process(1,rest-pick,jia+pick,yi).equals("甲")){
+                    return "甲";
+                }
+            }
+            return "乙";
+        }else{
+            for(int pick = 1;pick <= Math.min(rest,3);pick++){
+                if (process(0,rest-pick,jia,yi+pick).equals("乙")){
+                    return "乙";
+                }
+            }
+            return "甲";
+        }
+    }
+
+    //假设一开始的球数量不是21，是任意的正数
+    // 如果最终两个人拿的都是偶数，认为无人获胜，平局
+    // 如果最终两个人拿的都是奇数，认为无人获胜，平局
+    // rest代表目前剩下多少球
+    // cur == 0 代表目前是甲行动
+    // cur == 1 代表目前是乙行动
+    // first == 0 代表目前甲所选的球数，加起来是偶数
+    // first == 1 代表目前甲所选的球数，加起来是奇数
+    // second == 0 代表目前乙所选的球数，加起来是偶数
+    // second == 1 代表目前乙所选的球数，加起来是奇数
+    // 返回选完了rest个球，谁会赢，只会返回"甲"、"乙"、"平"
+    // win1方法，就是彻底暴力的做所有尝试，并且返回最终的胜利者
+    // 在甲的回合，甲会尝试所有的可能，以保证自己会赢，如果自己怎么都不会赢，那也要尽量平局，如果这个也不行，只能对方赢
+    // 在乙的回合，乙会尝试所有的可能，以保证自己会赢，如果自己怎么都不会赢，那也要尽量平局，如果这个也不行，只能对方赢\
+    public static String win1(int rest,int cur,int first,int second){
+        if (rest == 0) {
+            if (first==0&&second==1){
+                return "甲";
+            }
+            if (first==1&&second==0){
+                return "乙";
+            }
+            return "平";
+        }
+        if (cur == 0) {
+            String bestAns="乙";
+            for(int pick = 1;pick <= Math.min(3,rest);pick++){
+                String curAns = win1(rest-pick,1,first^(pick&1),second);
+                if (curAns.equals("甲")){
+                    bestAns="甲";
+                    break;
+                }
+                if (curAns.equals("平")){
+                    bestAns="平";
+                }
+            }
+            return bestAns;
+        }else{
+            String bestAns = "甲";
+            for(int pick = 1;pick <= Math.min(3,rest);pick++){
+                String curAns = win1(rest-pick,0,first,second^(pick&1));
+                if (curAns.equals("乙")){
+                    bestAns="乙";
+                    break;
+                }
+                if (curAns.equals("平")){
+                    bestAns="平";
+                }
+            }
+            return bestAns;
+        }
+    }
+
+    public static String[][][][]dp = new String[5000][2][2][2];
+    public static String win2(int rest,int cur,int first,int second){
+        if (rest == 0) {
+            if (first==0&&second==1){
+                return "甲";
+            }
+            if (first==1&&second==0){
+                return "乙";
+            }
+            return "平";
+        }
+
+        if (dp[rest][cur][first][second]!=null){
+            return dp[rest][cur][first][second];
+        }
+        if (cur == 0) {
+            String bestAns="乙";
+            for(int pick = 1;pick <= Math.min(3,rest);pick++){
+                String curAns = win1(rest-pick,1,first^(pick&1),second);
+                if (curAns.equals("甲")){
+                    bestAns="甲";
+                    break;
+                }
+                if (curAns.equals("平")){
+                    bestAns="平";
+                }
+            }
+            dp[rest][cur][first][second] = bestAns;
+            return bestAns;
+        }else{
+            String bestAns = "甲";
+            for(int pick = 1;pick <= Math.min(3,rest);pick++){
+                String curAns = win1(rest-pick,0,first,second^(pick&1));
+                if (curAns.equals("乙")){
+                    bestAns="乙";
+                    break;
+                }
+                if (curAns.equals("平")){
+                    bestAns="平";
+                }
+            }
+            dp[rest][cur][first][second] = bestAns;
+            return bestAns;
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int balls = 1; balls <= 500; balls += 2) {
+            System.out.println("球数为 " + balls + " 时 , 赢的是 " + win(balls));
+        }
+    }
 }
--- weekly-problems/src/class_2022_05_1/Code04_SumOfQuadraticSum.java
+++ weekly-problems/src/class_2022_05_1/Code04_SumOfQuadraticSum.java
@@ -1,8 +1,105 @@
 package class_2022_05_1;
+
 //来自学员问题，蓝桥杯练习题
 //f(i) : i的所有因子，每个因子都平方之后，累加起来
 //比如f(10) = 1平方 + 2平方 + 5平方 + 10平方 = 1 + 4 + 25 + 100 = 130
 //给定一个数n，求f(1) + f(2) + .. + f(n)
 //n <= 10的9次方
+// O(n)的方法都会超时！低于它的！
+// O(根号N)的方法，就过了，一个思路
+// O(log N)的方法，
 public class Code04_SumOfQuadraticSum {
+
+    public static long sum1(long n) {
+        //每个数作为因子出现的次数
+        int[] cnt = new int[(int) n + 1];//下标0抛弃不用
+        for (int num = 1; num <= n; num++) {
+            //内层循环寻找num的所有因子
+            for (int j = 1; j <= num; j++) {
+                if (num % j == 0) {
+                    cnt[j]++;
+                }
+            }
+        }
+        long ans = 0;
+        for (long i = 1; i <= n; i++) {
+            ans += i * i * (long) (cnt[(int) i]);
+        }
+        return ans;
+    }
+
+    public static long sum2(long n) {
+        //n的平方根，作为分段处理的临界点
+        long sqrt = (long) Math.pow((double) n, 0.5);
+        long ans = 0;
+        for (long i = 1; i <= sqrt; i++) {
+            ans += i * i * (n / i);
+        }
+        //k:二分出来的因子个数
+        for (long k = n / (sqrt + 1); k >= 1; k--) {
+            ans += sumOfLimitNumber(n, k);
+        }
+        return ans;
+    }
+
+    //v：确定因子的范围
+    //n：因子出现的次数
+    public static long sumOfLimitNumber(long v, long n) {
+        long r = cover(v, n);
+        long l = cover(v, n + 1);
+        // 计算[l+1, r]范围内所有数的平方和，再乘以k值(n)
+        // 平方和公式：1²+2²+...+m² = m*(m+1)*(2m+1)/6
+        return ((r * (r + 1) * ((r << 1) + 1) - l * (l + 1) * ((l << 1) + 1)) * n) / 6;
+    }
+
+    public static long cover(long v, long n) {
+        long l = 1;
+        long r = v;
+        long m = 0;
+        long ans = 0;
+        while (l <= r) {
+            m = (l + r) / 2;
+            if (m * n <= v) {
+                ans = m;
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        return ans;
+    }
+
+    public static void test(int n) {
+        int[] cnt = new int[n + 1];
+        for (int num = 1; num <= n; num++) {
+            for (int j = 1; j <= num; j++) {
+                if (num % j == 0) {
+                    cnt[j]++;
+                }
+            }
+        }
+        for (int i = 1; i <= n; i++) {
+            System.out.println("因子: " + i + "，个数:" + cnt[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+
+//		test(100);
+
+        System.out.println("测试开始");
+        for (long i = 1; i < 1000; i++) {
+            if (sum1(i) != sum2(i)) {
+                System.out.println("出错了!");
+            }
+        }
+        System.out.println("测试结束");
+
+        long n = 50000000000L; // 5 * 10的10次方
+        long start = System.currentTimeMillis();
+        sum2(n);
+        long end = System.currentTimeMillis();
+        System.out.println("大样本测试，n = " + n);
+        System.out.println("运行时间 : " + (end - start) + " ms");
+    }
 }
