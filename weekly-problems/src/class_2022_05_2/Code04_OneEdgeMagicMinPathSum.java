904bf6b6fe51112e6a096ae12bb5fcd771ec3a09
Wxw <weiwei_2024024@qq.com>
2025-10-14 10:40:59 +0800

    modify

--- weekly-problems/src/class_2022_05_2/Code04_OneEdgeMagicMinPathSum.java
+++ weekly-problems/src/class_2022_05_2/Code04_OneEdgeMagicMinPathSum.java
@@ -20,10 +20,10 @@ public class Code04_OneEdgeMagicMinPathSum {
                 graph.add(new ArrayList<>());
             }
             //对第i条边添加反向边
-            graph.get(roads[i][1]).add(new int[] { roads[i][0], roads[i][2] });
+            graph.get(roads[i][1]).add(new int[]{roads[i][0], roads[i][2]});
             //添加所有原始边
             for (int[] r : roads) {
-                graph.get(r[0]).add(new int[] { r[1], r[2] });
+                graph.get(r[0]).add(new int[]{r[1], r[2]});
             }
             ans = Math.min(ans, dijkstra1(n, graph));
         }
@@ -35,7 +35,7 @@ public class Code04_OneEdgeMagicMinPathSum {
         PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> a[1] - b[1]);
         boolean[] visited = new boolean[n + 1];
         //起点是1，初始距离为0
-        heap.add(new int[] { 1, 0 });
+        heap.add(new int[]{1, 0});
         int ans = Integer.MAX_VALUE;
         while (!heap.isEmpty()) {
             int[] cur = heap.poll();
@@ -51,14 +51,104 @@ public class Code04_OneEdgeMagicMinPathSum {
                 int to = edge[0];
                 int weight = edge[1];
                 if (!visited[to]) {
-                    heap.add(new int[] { to, cur[1] + weight });
+                    heap.add(new int[]{to, cur[1] + weight});
                 }
             }
         }
         return ans;
     }
 
-    public static int[][]randomRoads(int n,int v){
 
+    public static int min2(int n, int[][] roads) {
+        ArrayList<ArrayList<int[]>> graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for (int[] r : roads) {
+            graph.get(r[0]).add(new int[]{0, r[1], r[2]});
+            //1表示为反方向的边，1：表示已经使用魔法
+            graph.get(r[1]).add(new int[]{1, r[0], r[2]});
+        }
+        //[是否使用过魔法, 当前节点, 累计距离]
+        PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> a[2] - b[2]);
+        //访问标记数组：visited[0][u]表示未使用魔法到达u的状态已访问
+        //visited[1][u]表示已使用魔法到达u的状态已访问
+        boolean[][] visited = new boolean[2][n + 1];
+        heap.add(new int[]{0, 1, 0});
+        int ans = Integer.MAX_VALUE;
+        while (!heap.isEmpty()) {
+            int[] cur = heap.poll();
+            if (visited[cur[0]][cur[1]]) {
+                continue;
+            }
+            visited[cur[0]][cur[1]] = true;
+            if (cur[1] == n) {
+                ans = Math.min(ans, cur[2]);
+                if (visited[0][n] && visited[1][n]) {
+                    break;
+                }
+            }
+            for (int[] edge : graph.get(cur[1])) {
+                //未用魔法，且走的是原始方向的边
+                if (cur[0] + edge[0] == 0) {
+                    if (!visited[0][edge[1]]) {
+                        heap.add(new int[]{0, edge[1], cur[2] + edge[2]});
+                    }
+                }
+                //两种情况：未用魔法，走反方向的边、已用魔法，走原始边
+                if (cur[0] + edge[0] == 1) {
+                    if (!visited[1][edge[1]]) {
+                        heap.add(new int[]{1, edge[1], cur[2] + edge[2]});
+                    }
+                }
+            }
+        }
+        return ans == Integer.MAX_VALUE ? -1 : ans;
     }
+
+    public static int[][] randomRoads(int n, int v) {
+        int m = (int) (Math.random() * (n * (n - 1) / 2)) + 1;
+        int[][] roads = new int[m][3];
+        for (int i = 0; i < m; i++) {
+            roads[i][0] = (int) (Math.random() * n) + 1;
+            roads[i][1] = (int) (Math.random() * n) + 1;
+            roads[i][2] = (int) (Math.random() * v) + 1;
+        }
+        return roads;
+    }
+
+    // 为了测试
+    public static void main(String[] args) {
+        int N = 20;
+        int V = 30;
+        int testTime = 20000;
+        System.out.println("测试开始");
+        for (int i = 0; i < testTime; i++) {
+            int n = (int) (Math.random() * N) + 1;
+            int[][] roads = randomRoads(n, V);
+            if (min1(n, roads) != min2(n, roads)) {
+                System.out.println("出错了!");
+            }
+        }
+        System.out.println("测试结束");
+
+        // 点的数量10^5
+        int n = 100000;
+        // 边的数量2 * 10^5
+        int m = 200000;
+        // 时间复杂度很明显和边的权重是没关系的
+        // 所以这里设置小一点，防止出现溢出的解
+        int v = 100;
+        int[][] roads = new int[m][3];
+        for (int i = 0; i < m; i++) {
+            roads[i][0] = (int) (Math.random() * n) + 1;
+            roads[i][1] = (int) (Math.random() * n) + 1;
+            roads[i][2] = (int) (Math.random() * v) + 1;
+        }
+        long start = System.currentTimeMillis();
+        System.out.println("运行结果 : " + min2(n, roads));
+        long end = System.currentTimeMillis();
+        System.out.println("运行时间(毫秒) : " + (end - start));
+    }
+
 }
--- weekly-problems/src/class_2022_05_3/Code01_MaxNumberUnderLimit.java
+++ weekly-problems/src/class_2022_05_3/Code01_MaxNumberUnderLimit.java
@@ -1,4 +1,7 @@
 package class_2022_05_3;
+
+import java.util.Arrays;
+
 //来自字节
 //输入:
 //去重数组arr，里面的数只包含0~9
@@ -6,4 +9,158 @@ package class_2022_05_3;
 //返回:
 //要求比limit小的情况下，能够用arr拼出来的最大数字
 public class Code01_MaxNumberUnderLimit {
+
+    public static int tmp = 0;//在递归中记录找到的最大符合条件的数字
+
+    public static int maxNumber1(int[] arr, int limit) {
+        tmp = 0;
+        Arrays.sort(arr);
+        limit--;//关键处理：将限制值减1，转为"小于等于limit-1"
+        int offset = 1;//确定数字的位数
+        while (offset <= limit / 10) {
+            offset *= 10;
+        }
+        process1(arr, 0, offset, limit);
+        //取存储与rest小一位的数字
+        if (tmp == 0) {
+            int rest = 0;
+            offset /= 10;
+            while (offset > 0) {
+                rest += arr[arr.length - 1] * offset;
+                offset /= 10;
+            }
+            return rest;
+        }
+        return tmp;
+    }
+
+    //num：当前已拼接的数字
+    //offset:当前已处理的位数权重
+    public static void process1(int[] arr, int num, int offset, int limit) {
+        if (offset == 0) {
+            if (num <= limit) {
+                tmp = Math.max(tmp, num);
+            }
+        } else {
+            for (int cur : arr) {
+                process1(arr, num * 10 + cur, offset / 10, limit);
+            }
+        }
+    }
+
+    public static int maxNumber2(int[] arr, int limit) {
+        Arrays.sort(arr);
+        limit--;
+        int offset = 1;//计算最高位权重
+        while (offset <= limit / 10) {
+            offset *= 10;
+        }
+        //尝试拼接与limit相同位数的数字
+        int ans = process2(arr, limit, offset);
+        if (ans != -1) {
+            return ans;
+        //寻找少一位的最大位数
+        } else {
+            offset /= 10;
+            int rest = 0;
+            //只拼最大数
+            while (offset > 0) {
+                rest += arr[arr.length - 1] * offset;
+                offset /= 10;
+            }
+            return rest;
+        }
+    }
+    //每一步都去选最大可能的数字
+    public static int process2(int[] arr, int limit, int offset) {
+        if (offset == 0) {
+            return limit;
+        }
+        //提取当前位的目标值
+        int cur = (limit / offset) % 10;
+        //去找<=cur的最大数字
+        int near = near(arr, cur);
+        if (near == -1) {
+            return -1;
+        } else if (arr[near] == cur) {
+            int ans = process2(arr, limit, offset / 10);
+            if (ans != -1) {
+                return ans;
+                //无解，当前位选比cur小的
+            } else if (near > 0) {
+                near--;
+                //高位固定 + 当前位 + 后续位全最大（调用rest方法）
+                return (limit / (offset * 10)) * offset * 10 + (arr[near] * offset) + rest(arr, offset / 10);
+            } else {
+                return -1;
+            }
+        } else {
+            return (limit / (offset * 10)) * offset * 10 + (arr[near] * offset) + rest(arr, offset / 10);
+        }
+    }
+
+    public static int rest(int[] arr, int offset) {
+        int rest = 0;
+        while (offset > 0) {
+            rest += arr[arr.length - 1] * offset;
+            offset /= 10;
+        }
+        return rest;
+    }
+
+    public static int near(int[] arr, int num) {
+        int l = 0;
+        int r = arr.length - 1;
+        int m = 0;
+        int near = -1;
+        while (l <= r) {
+            m = (l + r) / 2;
+            if (arr[m] <= num) {
+                near = m;
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+        return near;
+    }
+
+    public static int[] randomArray() {
+        int[] arr = new int[(int) (Math.random() * 10) + 1];
+        boolean[] cnt = new boolean[10];
+        for (int i = 0; i < arr.length; i++) {
+            do {
+                arr[i] = (int) (Math.random() * 10);
+            } while (cnt[arr[i]]);
+            cnt[arr[i]] = true;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int max = 3000;
+        int testTime = 100;
+        System.out.println("测试开始");
+        for (int i = 0; i < max; i++) {
+            int[] arr = randomArray();
+            for (int j = 0; j < testTime; j++) {
+                int ans1 = maxNumber1(arr, i);
+                int ans2 = maxNumber2(arr, i);
+                if (ans1 != ans2) {
+                    System.out.println("出错了!");
+                    System.out.println("数组为 ：");
+                    for (int num : arr) {
+                        System.out.print(num + " ");
+                    }
+                    System.out.println();
+                    System.out.println("数字为 ：" + i);
+                    System.out.println(ans1);
+                    System.out.println(ans2);
+                }
+            }
+        }
+        System.out.println("测试结束");
+
+    }
+
 }
