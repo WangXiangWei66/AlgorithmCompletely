9aae1c4e2c1db4e559e9b547fddfe9c47504529d
Wxw <weiwei_2024024@qq.com>
2025-10-15 11:09:15 +0800

    modify

--- weekly-problems/src/class_2022_05_3/Code02_RemoveNumbersNotIncreasingAll.java
+++ weekly-problems/src/class_2022_05_3/Code02_RemoveNumbersNotIncreasingAll.java
@@ -1,4 +1,5 @@
 package class_2022_05_3;
+
 //来自京东
 //4.2笔试
 //给定一个数组arr，长度为N，arr中所有的值都在1~K范围上
@@ -6,4 +7,146 @@ package class_2022_05_3;
 //返回至少删除几个数字能达到目的
 //N <= 10^4，K <= 10^2
 public class Code02_RemoveNumbersNotIncreasingAll {
+
+    public static int minRemove1(int[] arr, int k) {
+        return process1(arr, 0, new int[arr.length], 0, k);
+    }
+
+    //size：路径的大小
+    public static int process1(int[] arr, int index, int[] path, int size, int k) {
+        if (index == arr.length) {
+            return lengthOfLIS(path, size) < k ? (arr.length - size) : Integer.MAX_VALUE;
+        } else {
+            int p1 = process1(arr, index + 1, path, size, k);
+            path[size] = arr[index];
+            int p2 = process1(arr, index + 1, path, size + 1, k);
+            return Math.min(p1, p2);
+        }
+    }
+
+    //计算最长递增子序列
+    public static int lengthOfLIS(int[] arr, int size) {
+        if (size == 0) {
+            return 0;
+        }
+        //保存不同长度的递增子序列的最小可能尾部值
+        int[] ends = new int[size];
+        ends[0] = arr[0];
+        int right = 0;//ends数组中有效元素的右边界
+        int l = 0;
+        int r = 0;
+        int m = 0;
+        int max = 1;//最长递增子序列长度
+        for (int i = 1; i < size; i++) {
+            l = 0;
+            r = right;
+            while (l <= r) {
+                m = (l + r) / 2;
+                if (arr[i] > ends[m]) {
+                    l = m + 1;
+                } else {
+                    r = m - 1;
+                }
+            }
+            right = Math.max(right, l);
+            ends[l] = arr[i];
+            max = Math.max(max, l + 1);
+        }
+        return max;
+    }
+
+    //len：当前已形成的递增子序列长度
+    public static int wang(int[] arr, int index, int len, int k) {
+        if (len == k) {
+            return Integer.MAX_VALUE;
+        }
+        if (index == arr.length) {
+            return 0;
+        }
+        int cur = arr[index];
+        if (len >= cur || len + 1 < cur) {
+            return wang(arr, index + 1, len, k);
+        }
+        //将当前元素加入子序列
+        int p1 = wang(arr, index + 1, len + 1, k);
+        int p2 = Integer.MAX_VALUE;
+        //删除当前元素
+        int next2 = wang(arr, index + 1, len, k);
+        if (next2 != Integer.MAX_VALUE) {
+            p2 = 1 + next2;
+        }
+        return Math.min(p1, p2);
+    }
+
+    public static int minRemove2(int[] arr, int k) {
+        int n = arr.length;
+        // dp[i][j]表示：处理到第i个元素，当前递增子序列长度为j时，需要的最少删除数
+        int[][] dp = new int[n][k];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < k; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        return process2(arr, k, 0, 0, dp);
+    }
+
+    public static int process2(int[] arr, int k, int index, int range, int[][] dp) {
+        if (range == k) {
+            return Integer.MAX_VALUE;
+        }
+        if (index == arr.length) {
+            return 0;
+        }
+        if (dp[index][range] != -1) {
+            return dp[index][range];
+        }
+        int ans = 0;
+        //当前元素正好可以延长递增子序列
+        if (arr[index] == range + 1) {
+            //删除当前元素
+            int p1 = process2(arr, k, index + 1, range, dp);
+            p1 += p1 != Integer.MAX_VALUE ? 1 : 0;
+            int p2 = process2(arr, k, index + 1, range + 1, dp);
+            ans = Math.min(p1, p2);
+        } else {
+            ans = process2(arr, k, index + 1, range, dp);
+        }
+        dp[index][range] = ans;
+        return ans;
+    }
+
+    public static int[] randomArray(int len, int k) {
+        int[] arr = new int[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = (int) (Math.random() * k) + 1;
+        }
+        return arr;
+    }
+
+    // 为了验证
+    public static void main(String[] args) {
+        int N = 15;
+        int K = 6;
+        int testTime = 10000;
+        System.out.println("测试开始");
+        for (int i = 0; i < testTime; i++) {
+            int len = (int) (Math.random() * N) + 1;
+            int k = (int) (Math.random() * K) + 1;
+            int[] arr = randomArray(len, k);
+            int ans1 = minRemove1(arr, k);
+            int ans2 = minRemove2(arr, k);
+            if (ans1 != ans2) {
+                System.out.println("出错了！");
+                for (int num : arr) {
+                    System.out.print(num + " ");
+                }
+                System.out.println();
+                System.out.println("k : " + k);
+                System.out.println("ans1 : " + ans1);
+                System.out.println("ans2 : " + ans2);
+                break;
+            }
+        }
+        System.out.println("测试结束");
+    }
 }
--- weekly-problems/src/class_2022_05_3/Code04_MinJumpUsePre.java
+++ weekly-problems/src/class_2022_05_3/Code04_MinJumpUsePre.java
@@ -1,9 +1,12 @@
 package class_2022_05_3;
+
+import java.util.Arrays;
+
 //来自学员问题
 //为了给刷题的同学一些奖励，力扣团队引入了一个弹簧游戏机
 //游戏机由 N 个特殊弹簧排成一排，编号为 0 到 N-1
 //初始有一个小球在编号 0 的弹簧处。若小球在编号为 i 的弹簧处
-//通过按动弹簧，可以选择把小球向右弹射 jump[i] 的距离，或者向左弹射到任意左侧弹簧的位置
+//通过按动弹簧，可以选择把小球向右弹射jump[i] 的距离，或者向左弹射到任意左侧弹簧的位置
 //也就是说，在编号为 i 弹簧处按动弹簧，
 //小球可以弹向 0 到 i-1 中任意弹簧或者 i+jump[i] 的弹簧（若 i+jump[i]>=N ，则表示小球弹出了机器）
 //小球位于编号 0 处的弹簧时不能再向左弹。
@@ -11,4 +14,143 @@ package class_2022_05_3;
 //请求出最少需要按动多少次弹簧，可以将小球从编号 0 弹簧弹出整个机器，即向右越过编号 N-1 的弹簧。
 //测试链接 : https://leetcode-cn.com/problems/zui-xiao-tiao-yue-ci-shu/
 public class Code04_MinJumpUsePre {
+
+    public int minJump(int[] jump) {
+        int n = jump.length;
+        //用数组实现队列，存储待处理的弹簧的位置
+        int[] queue = new int[n];
+        int l = 0;
+        int r = 0;
+        //初始时，小球在0位置
+        queue[r++] = 0;
+        IndexTree it = new IndexTree(n);
+        //初始，1~n-1号弹簧都是为访问过的
+        for (int i = 1; i < n; i++) {
+            it.add(i, 1);
+        }
+        int step = 0;//按动次数
+        //当前步骤可达的所有位置
+        while (l != r) {
+            int tmp = r;//当前轮次的队列尾
+            for (; l < tmp; l++) {
+                int cur = queue[l];
+                int forward = cur + jump[cur];
+                if (forward >= n) {
+                    return step + 1;
+                }
+                //目标位置没有访问过
+                if (it.value(forward) != 0) {
+                    queue[r++] = forward;
+                    it.add(forward, -1);
+                }
+                //往左弹射
+                while (it.sum(cur - 1) != 0) {
+                    //最左侧的未访问位置
+                    int find = find(it, cur - 1);
+                    it.add(find, -1);
+                    queue[r++] = find;
+                }
+            }
+            step++;
+        }
+        return -1;
+    }
+
+    // 在[0, right]范围内找到第一个未访问的位置（二分查找）
+    public static int find(IndexTree it, int right) {
+        int left = 0;
+        int mid = 0;
+        int find = 0;
+        while (left <= right) {
+            mid = (left + right) / 2;
+            if (it.sum(mid) > 0) {
+                find = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return find;
+    }
+
+
+    //解决前缀和查询和单点更新
+    public static class IndexTree {
+        //存储实际数量的树的底层数组
+        private int[] tree;
+        private int N;
+
+        public IndexTree(int size) {
+            N = size;
+            tree = new int[N + 1];
+        }
+
+        //获取指定索引位置的值
+        public int value(int index) {
+            if (index == 0) {
+                return sum(0);
+            } else {
+                return sum(index) - sum(index - 1);
+            }
+        }
+
+        //计算前i个元素的前缀和
+        public int sum(int i) {
+            //转化为索引树的内部索引
+            int index = i + 1;
+            int ret = 0;
+            while (index > 0) {
+                //减去二进制中最后一个1所代表的值
+                ret += tree[index];
+                index -= index & -index;
+            }
+            return ret;
+        }
+
+        public void add(int i, int d) {
+            int index = i + 1;
+            while (index <= N) {
+                tree[index] += d;
+                index += index & -index;
+            }
+        }
+    }
+
+    public int minJump2(int[] jump) {
+        int N = jump.length;
+        int ans = N;
+        //从0号弹簧往右跳
+        int next = jump[0];
+        if (next >= N) {
+            return 1;
+        }
+        if (next + jump[next] >= N) {
+            return 2;
+        }
+        //dp[i]:到达i号弹簧所需的最小步数
+        int[] dp = new int[N + 1];
+        Arrays.fill(dp, N);
+        //dis数组记录每步能到达的最远距离
+        int[] dis = new int[N];
+        dis[1] = next;
+        dis[2] = next + jump[next];
+        dp[next + jump[next]] = 2;
+        int step = 1;//当前步数
+        for (int i = 1; i < N; i++) {
+            //当前位置超出了当前步数能到达的最远距离
+            if (i > dis[step]) {
+                step++;
+            }
+            dp[i] = Math.min(dp[i], step + 1);
+            next = i + jump[i];
+            if (next >= N) {
+                ans = Math.min(ans, dp[i] + 1);
+            //更新目标位置的最小步数
+            } else if (dp[next] > dp[i] + 1) {
+                dp[next] = dp[i] + 1;
+                dis[dp[next]] = Math.max(dis[dp[next]], next);
+            }
+        }
+        return ans;
+    }
 }
