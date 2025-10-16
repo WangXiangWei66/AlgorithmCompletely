96cf4f98aefea447c03c9e05d16b7c283e2e3854
Wxw <weiwei_2024024@qq.com>
2025-10-16 09:59:54 +0800

    modify

--- weekly-problems/src/class_2022_05_4/Code01_SomeDPFromVT.java
+++ weekly-problems/src/class_2022_05_4/Code01_SomeDPFromVT.java
@@ -1,7 +1,174 @@
 package class_2022_05_4;
+
+// 来自弗吉尼亚理工大学(VT)，算法考试卷
+// 精选了还可以的几道题
+// 这些都是简单难度的动态规划，是面试中最常见的难度
+// 这几个题都有一些非常小的常见技巧可说
+//题目一：
 //每一个方案都有两个值(a,b)，a为娱乐值，b为进攻值
 //给定很多的方案数组，给定娱乐值总目标(int)，给定进攻值总目标(int)
 //要求选一些方案，这些方案的总娱乐值>=娱乐值总目标，总进攻值>=进攻值总目标
 //返回至少需要选几个方案
 public class Code01_SomeDPFromVT {
+
+    public static int minStickers1(int[][] stickers, int funnyGoal, int offenseGoal) {
+        return process1(stickers, 0, funnyGoal, offenseGoal);
+    }
+
+    public static int process1(int[][] stickers, int index, int restFunny, int restOffense) {
+        if (restFunny <= 0 && restOffense <= 0) {
+            return 0;
+        }
+        if (index == stickers.length) {
+            return Integer.MAX_VALUE;
+        }
+        int p1 = process1(stickers, index + 1, restFunny, restOffense);
+        int p2 = Integer.MAX_VALUE;
+        int next2 = process1(stickers, index + 1, Math.max(0, restFunny - stickers[index][0]), Math.max(0, restOffense - stickers[index][1]));
+        if (p2 != Integer.MAX_VALUE) {
+            p2 = next2 + 1;
+        }
+        return Math.min(p1, p2);
+    }
+
+    public static int minStickers2(int[][] stickers, int funnyGoal, int offenseGoal) {
+        int[][][] dp = new int[stickers.length][funnyGoal + 1][offenseGoal + 1];
+        for (int i = 0; i < stickers.length; i++) {
+            for (int j = 0; j <= funnyGoal; j++) {
+                for (int k = 0; k <= offenseGoal; k++) {
+                    dp[i][j][k] = -1;
+                }
+            }
+        }
+        return process2(stickers, 0, funnyGoal, offenseGoal, dp);
+    }
+
+    public static int process2(int[][] stickers, int index, int restFunny, int restOffense, int[][][] dp) {
+        if (restFunny <= 0 && restOffense <= 0) {
+            return 0;
+        }
+        if (index == stickers.length) {
+            return Integer.MAX_VALUE;
+        }
+        if (dp[index][restFunny][restOffense] != -1) {
+            return dp[index][restFunny][restOffense];
+        }
+        int p1 = process2(stickers, index + 1, restFunny, restOffense, dp);
+        int p2 = Integer.MAX_VALUE;
+        int next2 = process2(stickers, index + 1, Math.max(0, restFunny - stickers[index][0]), Math.max(0, restOffense - stickers[index][1]), dp);
+        if (next2 != Integer.MAX_VALUE) {
+            p2 = next2 + 1;
+        }
+        int ans = Math.min(p1, p2);
+        dp[index][restFunny][restOffense] = ans;
+        return ans;
+    }
+
+    public static int minStickers3(int[][] stickers, int funnyGoal, int offenseGoal) {
+        int n = stickers.length;
+        int[][][] dp = new int[n + 1][funnyGoal + 1][offenseGoal + 1];
+        for (int f = 0; f <= funnyGoal; f++) {
+            for (int o = 0; o <= offenseGoal; o++) {
+                if (f != 0 || o != 0) {
+                    dp[n][f][0] = Integer.MAX_VALUE;
+                }
+            }
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            for (int f = 0; f <= funnyGoal; f++) {
+                for (int o = 0; o <= offenseGoal; o++) {
+                    if (f != 0 || o != 0) {
+                        int p1 = dp[i + 1][f][0];
+                        int p2 = Integer.MAX_VALUE;
+                        int next2 = dp[i + 1][Math.max(0, f - stickers[i][0])][Math.max(0, o - stickers[i][1])];
+                        if (next2 != Integer.MAX_VALUE) {
+                            p2 = next2 + 1;
+                        }
+                        dp[i][f][o] = Math.min(p1, p2);
+                    }
+                }
+            }
+        }
+        return dp[0][funnyGoal][offenseGoal];
+    }
+
+
+    // 题目2
+// 绳子总长度为M
+// 100 -> M
+// (6, 100) (7,23) (10,34) -> arr
+// 每一个长度的绳子对应一个价格，比如(6, 10)表示剪成长度为6的绳子，对应价格10
+// 可以重复切出某个长度的绳子
+// 定义递归如下：
+// 所有可以切出来的长度 对应 价值都在数组ropes里
+// ropes[i] = {6, 10} 代表i方案为：切出长度为6的绳子，可以卖10元
+// index....所有的方案，随便选择。index之前的方案，不能选择
+// 返回最大的价值
+// arr[i][0] -> i号方案能切多少长度
+// arr[i][1] -> 切出来这个长度，就能获得的价值
+// arr[index....]自由选择，绳子还剩restLen长度
+// 返回，最大价值
+    public static int maxValue(int[][] arr, int index, int restLen) {
+        if (restLen <= 0 || index == arr.length) {
+            return 0;
+        }
+        int p1 = maxValue(arr, index + 1, restLen);
+        int p2 = 0;
+        if (arr[index][0] <= restLen) {
+            //方案是可以重复选的
+            p2 = arr[index][1] + maxValue(arr, index, restLen - arr[index][0]);
+        }
+        return Math.max(p1, p2);
+    }
+
+// 题目3
+// 每一个序列都是[a,b]的形式，a < b
+// 序列连接的方式为，前一个序列的b，要等于后一个序列的a
+// 比如 : [3, 7]、[7, 13]、[13, 26]这三个序列就可以依次连接
+// 给定若干个序列，求最大连接的数量
+// 定义尝试过程如下
+// arr[i] = {4, 9}表示，第i个序列4开始，9结束
+// pre : 代表选择的上一个序列，的，index是多少
+// 比如选择的上一个序列如果是(4,9)，是第5个序列，那么pre==5
+// 特别注意：如果从来没有选过序列，那么pre == -1
+// 这个函数含义 :
+// index....所有的序列，随便选择。index之前的序列，不能选择
+// 上一个选择的序列，是pre号，如果pre==-1,说明之前没有选择过序列
+// 返回题目要求的那种连接方式下，最大的序列数量
+// [5,13] [1,19] [2, 3] [79, 81] ...
+// [1,19] [2, 3] [5, 13] [79, 81]
+// arr[i][0] : 开头
+// arr[i][1] : 结尾
+// arr已经根据开头排过序了！
+// preEnd index
+// [1, 3] [2, 4] [4, 7]
+// 0 1 2
+// maxLen(0, -1)
+// 0（选） -> maxLen(1, 0)
+// 在arr[index...]选择序列，之前选的，离index最近的序列，位置在preIndex
+// 请返回，index...能链接起来的，序列数量的最大值
+
+    public static int maxLen(int[][] arr, int index, int preIndex) {
+        if (index == arr.length) {
+            return 0;
+        }
+        int p1 = maxLen(arr, index + 1, preIndex);
+        int p2 = 0;
+        if (arr[preIndex][1] == arr[index][0]) {
+            p2 = 1 + maxLen(arr, index + 1, index);
+        }
+        return Math.max(p1, p2);
+    }
+
+    public static int maxNumberSubsequence(int[][] arr, int index, int pre) {
+        if (index == arr.length) {
+            return 0;
+        }
+        int p1 = maxNumberSubsequence(arr, index + 1, pre);
+        int p2 = -1;
+        if (pre == -1 || arr[pre][1] == arr[index][0]) {
+            p2 = 1 + maxNumberSubsequence(arr, index + 1, index);
+        }
+        return Math.max(p1, p2);
+    }
 }
--- weekly-problems/src/class_2022_05_4/Code03_MaxIncreasingSubarrayCanDeleteContinuousPart.java
+++ weekly-problems/src/class_2022_05_4/Code03_MaxIncreasingSubarrayCanDeleteContinuousPart.java
@@ -1,8 +1,216 @@
 package class_2022_05_4;
+
+import java.util.Arrays;
+
 //来自字节
 //5.6笔试
 //给定一个数组arr，长度为n，最多可以删除一个连续子数组，
 //求剩下的数组，严格连续递增的子数组最大长度
 //n <= 10^6
 public class Code03_MaxIncreasingSubarrayCanDeleteContinuousPart {
+
+    public static int maxLen1(int[] arr) {
+        int ans = max(arr);//原数组最长递增子序列长度
+        int n = arr.length;
+        for (int L = 0; L < n; L++) {
+            for (int R = L; R < n; R++) {
+                int[] cur = delete(arr, L, R);
+                ans = Math.max(ans, max(cur));
+            }
+        }
+        return ans;
+    }
+
+    public static int[] delete(int[] arr, int L, int R) {
+        int n = arr.length;
+        //新数组长度
+        int[] ans = new int[n - (R - L + 1)];
+        //新数组索引
+        int index = 0;
+        //将删除区间外的元素重新复制
+        for (int i = 0; i < L; i++) {
+            ans[index++] = arr[i];
+        }
+        for (int i = R + 1; i < n; i++) {
+            ans[index++] = arr[i];
+        }
+        return ans;
+    }
+
+    public static int max(int[] arr) {
+        if (arr.length == 0) {
+            return 0;
+        }
+        int ans = 1;
+        int cur = 1;//当前连续递增子数组长度
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > arr[i - 1]) {
+                cur++;
+            } else {
+                cur = 1;
+            }
+            ans = Math.max(ans, cur);
+        }
+        return ans;
+    }
+
+    public static class SegmentTree {
+        private int n;
+        private int[] max;
+        private int[] update;
+
+        public SegmentTree(int maxSize) {
+            n = maxSize + 1;
+            max = new int[n << 2];
+            update = new int[n << 2];
+            Arrays.fill(update, -1);
+        }
+
+        public int get(int index) {
+            return max(index, index, 1, n, 1);
+        }
+
+        public void update(int index, int c) {
+            update(index, index, c, 1, n, 1);
+        }
+
+        public int max(int right) {
+            return max(1, right, 1, n, 1);
+        }
+
+        private void pushUp(int rt) {
+            max[rt] = Math.max(max[rt << 1], max[rt << 1 | 1]);
+        }
+
+        private void pushDown(int rt, int ln, int rn) {
+            if (update[rt] != -1) {
+                update[rt << 1] = update[rt];
+                max[rt << 1] = update[rt];
+                update[rt << 1 | 1] = update[rt];
+                max[rt << 1 | 1] = update[rt];
+                update[rt] = -1;
+            }
+        }
+
+        private void update(int L, int R, int C, int l, int r, int rt) {
+            if (L <= l && r <= R) {
+                max[rt] = C;
+                update[rt] = C;
+                return;
+            }
+            int mid = (l + r) >> 1;
+            pushDown(rt, mid - l + 1, r - mid);
+            if (L <= mid) {
+                update(L, R, C, l, mid, rt << 1);
+            }
+            if (R > mid) {
+                update(L, R, C, mid + 1, r, rt << 1 | 1);
+            }
+            pushUp(rt);
+        }
+
+        public int max(int L, int R, int l, int r, int rt) {
+            if (L <= l && r <= R) {
+                return max[rt];
+            }
+            int mid = (l + r) >> 1;
+            pushDown(rt, mid - l + 1, r - mid);
+            int ans = 0;
+            if (L <= mid) {
+                ans = Math.max(ans, max(L, R, l, mid, rt << 1));
+            }
+            if (R > mid) {
+                ans = Math.max(ans, max(L, R, mid + 1, r, rt << 1 | 1));
+            }
+            return ans;
+        }
+    }
+
+    public static int maxLen2(int[] arr) {
+        if (arr.length == 0) {
+            return 0;
+        }
+        int n = arr.length;
+        int[] sorted = new int[n];
+        for (int i = 0; i < n; i++) {
+            sorted[i] = arr[i];
+        }
+        Arrays.sort(sorted);
+        SegmentTree st = new SegmentTree(n);
+        //将第一个元素排名位置更新为1
+        st.update(rank(sorted, arr[0]), 1);
+        //dp数组：dp[i]表示以i位置结尾的最长严格递增子数组长度
+        int[] dp = new int[n];
+        dp[0] = 1;
+        int ans = 1;
+        int cur = 1;
+        for (int i = 1; i < n; i++) {
+            //获取当前数组的排名
+            int rank = rank(sorted, arr[i]);
+            //不删除某些元素
+            int p1 = arr[i - 1] < arr[i] ? (dp[i - 1] + 1) : 1;
+            //删除某些元素
+            int p2 = rank > 1 ? (st.max(rank - 1) + 1) : 1;
+            dp[i] = Math.max(p1, p2);
+            ans = Math.max(ans, dp[i]);
+            if (arr[i] > arr[i - 1]) {
+                cur++;
+            } else {
+                cur = 1;
+            }
+            if (st.get(rank) < cur) {
+                st.update(rank, cur);
+            }
+        }
+        return ans;
+    }
+
+    public static int rank(int[] sorted, int num) {
+        int l = 0;
+        int r = sorted.length - 1;
+        int m = 0;
+        int ans = -1;
+        while (l <= r) {
+            m = (l + r) / 2;
+            if (sorted[m] >= num) {
+                ans = m;
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+        return ans + 1;
+    }
+
+    public static int[] randomArray(int len, int v) {
+        int[] arr = new int[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = (int) (Math.random() * v) - (int) (Math.random() * v);
+        }
+        return arr;
+    }
+
+    // 为了验证
+    public static void main(String[] args) {
+        int n = 100;
+        int v = 20;
+        int testTime = 5000;
+        System.out.println("测试开始");
+        for (int i = 0; i < testTime; i++) {
+            int m = (int) (Math.random() * n);
+            int[] arr = randomArray(m, v);
+            int ans1 = maxLen1(arr);
+            int ans2 = maxLen2(arr);
+            if (ans1 != ans2) {
+                for (int num : arr) {
+                    System.out.print(num + " ");
+                }
+                System.out.println();
+                System.out.println(ans1);
+                System.out.println(ans2);
+                break;
+            }
+        }
+        System.out.println("测试结束");
+    }
 }
