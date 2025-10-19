7b8d7e2f976c6c3364c81ba16db89e7825c370d2
Wxw <weiwei_2024024@qq.com>
2025-10-19 10:10:04 +0800

    modify

--- weekly-problems/src/class_2022_06_2/Code02_SumOfTotalStrengthOfWizards.java
+++ weekly-problems/src/class_2022_06_2/Code02_SumOfTotalStrengthOfWizards.java
@@ -1,12 +1,55 @@
 package class_2022_06_2;
 //作为国王的统治者，你有一支巫师军队听你指挥。
-//给你一个下标从 0 开始的整数数组 strength ，
-//其中 strength[i] 表示第 i 位巫师的力量值。
-//对于连续的一组巫师（也就是这些巫师的力量值是 strength 的 子数组），
-//总力量 定义为以下两个值的 乘积 ：
-//巫师中 最弱 的能力值 * 组中所有巫师的个人力量值 之和 。
-//请你返回 所有 巫师组的 总 力量之和。由于答案可能很大，请将答案对 109 + 7 取余 后返回。
-//子数组 是一个数组里 非空 连续子序列。
+//给你一个下标从 0开始的整数数组strength，
+//其中strength[i]表示第i位巫师的力量值。
+//对于连续的一组巫师（也就是这些巫师的力量值是strength的子数组），
+//总力量定义为以下两个值的乘积：
+//巫师中 最弱的能力值 * 组中所有巫师的个人力量值 之和。
+//请你返回 所有巫师组的 总力量之和。由于答案可能很大，请将答案对109 + 7取余后返回。
+//子数组是一个数组里 非空连续子序列。
 //测试链接 : https://leetcode.cn/problems/sum-of-total-strength-of-wizards/
 public class Code02_SumOfTotalStrengthOfWizards {
+
+    public static final long mod = 1000000007;
+
+    public static int totalStrength(int[] arr) {
+        int n = arr.length;
+        long preSum = arr[0];//记录当前的累加和（前缀和）
+        long[] sumSum = new long[n];//前缀和之和数组
+        sumSum[0] = arr[0];
+        for (int i = 1; i < n; i++) {
+            preSum += arr[i];
+            sumSum[i] = (sumSum[i - 1] + preSum) % mod;
+        }
+        //维持栈内单调递增的数组下标
+        int[] stack = new int[n];
+        int size = 0;//栈的实际大小
+        long ans = 0;
+        for (int i = 0; i < n; i++) {
+            while (size > 0 && arr[stack[size - 1]] >= arr[i]) {
+                int m = stack[--size];//栈顶元素弹出
+                int l = size > 0 ? stack[size - 1] : -1;
+                ans += magicSum(arr, sumSum, l, m, i);//计算以m为最小值的子数组的总力量
+                ans %= mod;
+            }
+            stack[size++] = i;
+        }
+        while (size > 0) {
+            int m = stack[--size];
+            int l = size > 0 ? stack[size - 1] : -1;
+            ans += magicSum(arr, sumSum, l, m, n);
+            ans %= mod;
+        }
+        return (int) ans;
+    }
+    //m为最小值，左边界l到右边界r之间的所有子数组的总力量之和
+    public static long magicSum(int[] arr, long[] sumSum, int l, int m, int r) {
+        //计算左部分：所有包含m且右边界在[m, r-1]的子数组的和的总和
+        //公式推导：(m - l) * (sumSum[r-1] - sumSum[m-1])
+        long left = (long) (m - l) * (sumSum[r - 1] - (m - 1 >= 0 ? sumSum[m - 1] : 0) + mod) % mod;
+        // 计算右部分：所有包含m且左边界在[l+1, m]的子数组的和的总和
+        // 公式推导：(r - m) * (sumSum[m-1] - sumSum[l-1])
+        long right = (long) (r - m) * ((m - 1 >= 0 ? sumSum[m - 1] : 0) - (l - 1 >= 0 ? sumSum[l - 1] : 0) + mod) % mod;
+        return (long) arr[m] * ((left - right + mod) % mod);
+    }
 }
--- weekly-problems/src/class_2022_06_2/Code03_NumberOfDifferentSubsequencesGCDs.java
+++ weekly-problems/src/class_2022_06_2/Code03_NumberOfDifferentSubsequencesGCDs.java
@@ -1,4 +1,5 @@
 package class_2022_06_2;
+
 //给你一个由正整数组成的数组 nums 。
 //数字序列的 最大公约数 定义为序列中所有整数的共有约数中的最大整数。
 //例如，序列 [4,6,16] 的最大公约数是 2 。
@@ -7,4 +8,40 @@ package class_2022_06_2;
 //计算并返回 nums 的所有 非空 子序列中 不同 最大公约数的 数目 。
 //测试链接 : https://leetcode.com/problems/number-of-different-subsequences-gcds/
 public class Code03_NumberOfDifferentSubsequencesGCDs {
+
+    public static int countDifferentSubsequenceGCDs(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        for (int num : nums) {
+            max = Math.max(max, num);
+        }
+        boolean[] set = new boolean[max + 1];
+        for (int num : nums) {
+            set[num] = true;
+        }
+        //计算所有可能成为gcd的数字
+        int ans = 0;
+        for (int a = 1; a <= max; a++) {
+            int g = a;
+            for (; g <= max; g += a) {
+                //找到了第一个
+                if (set[g]) {
+                    break;
+                }
+            }
+            for (int b = g; b <= max; b += a) {
+                if (set[b]) {
+                    g = gcd(g, b);
+                    if (g == a) {
+                        ans++;
+                        break;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static int gcd(int m, int n) {
+        return n == 0 ? m : gcd(n, m % n);
+    }
 }
