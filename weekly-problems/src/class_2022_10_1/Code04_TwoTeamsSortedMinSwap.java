1f719cbd1b1007796672237e1a762f180423a0bf
Wxw <weiwei_2024024@qq.com>
2025-12-02 10:18:49 +0800

    modify

--- weekly-problems/src/class_2022_10_1/Code04_TwoTeamsSortedMinSwap.java
+++ weekly-problems/src/class_2022_10_1/Code04_TwoTeamsSortedMinSwap.java
@@ -1,4 +1,7 @@
 package class_2022_10_1;
+
+import java.util.HashMap;
+
 //来自美团
 //两种颜色的球，蓝色和红色，都按1～n编号，共计2n个
 //为方便放在一个数组中，红球编号取负，篮球不变，并打乱顺序，
@@ -9,5 +12,114 @@ package class_2022_10_1;
 //[1,2,3,-1,-2,-3,-4,4]
 //最少交换次数为10
 //n <= 1000
+//递归+回溯+树状数组(使用贪心选择+代价计算)
 public class Code04_TwoTeamsSortedMinSwap {
+
+    // [3,-3,1,-4,2,-2,-1,4]
+    //    -3   -4   -2 -1   -> -1 -2 -3 -4
+    //  3    1    2       4 ->  1  2  3  4
+
+    // 这个题写对数器太麻烦了
+    // 所以这就是正式解
+    public static int minSwaps(int[] arr) {
+        int n = arr.length;
+        //键为篮球的编号,值为该编号在数组中的下标
+        HashMap<Integer, Integer> map = new HashMap<>();
+        //篮球和红球的最大编号
+        int topA = 0;
+        int topB = 0;
+        //寻找两个球的最大编号(负数取绝对值)
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > 0) {
+                topA = Math.max(topA, arr[i]);
+            } else {
+                topB = Math.max(topB, Math.abs(arr[i]));
+            }
+            //记录每个球的原始下标
+            map.put(arr[i], i);
+        }
+        IndexTree it = new IndexTree(n);
+        //初始化树状数组的值为 1 ,表示未占用
+        for (int i = 0; i < n; i++) {
+            it.add(i, 1);
+        }
+        return f(topA, topB, it, n - 1, map);
+    }
+
+    // 可以改二维动态规划！
+    // 因为it的状态，只由topA和topB决定
+    // 所以it的状态不用作为可变参数！
+    // TopA:当前剩余位置篮球的最大编号
+    //TopB:当前剩余位置红球的最大编号
+    public static int f(int topA, int topB,
+                        IndexTree it, int end,
+                        HashMap<Integer, Integer> map) {
+        if (topA == 0 && topB == 0) {
+            return 0;
+        }
+        int p1 = Integer.MAX_VALUE;
+        int p2 = Integer.MAX_VALUE;
+        int index, cost, next;
+        if (topA != 0) {
+            //获取TopA号篮球的原始下标
+            index = map.get(topA);
+            //移动代价
+            cost = it.sum(index, end) - 1;
+            //标记该位置已经被占用
+            it.add(index, -1);
+            //处理下一个篮球
+            next = f(topA - 1, topB, it, end, map);
+            it.add(index, 1);
+            p1 = cost + next;
+        }
+        if (topB != 0) {
+            index = map.get(-topB);
+            cost = it.sum(index, end) - 1;
+            it.add(index, -1);
+            next = f(topA, topB - 1, it, end, map);
+            it.add(index, 1);
+            p2 = cost + next;
+        }
+        return Math.min(p1, p2);
+    }
+
+    public static class IndexTree {
+        //树状数组的下标要保证从1开始
+        private int[] tree;
+        private int N;
+
+        public IndexTree(int size) {
+            N = size;
+            tree = new int[N + 1];
+        }
+        //数组中「下标为 i」的位置增加 v（单点更新）
+        public void add(int i, int v) {
+            //树状数组的下标是从1开始的
+            i++;
+            while (i <= N) {
+                tree[i] += v;
+                //找到当前节点的父节点
+                i += i & -i;
+            }
+        }
+
+        public int sum(int l, int r) {
+            return l == 0 ? sum(r + 1) : (sum(r + 1) - sum(l));
+        }
+        //前缀和查询方法
+        private int sum(int index) {
+            int ans = 0;
+            while (index > 0) {
+                ans += tree[index];
+                index -= index & -index;
+            }
+            return ans;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 3, -3, 1, -4, 2, -2, -1, 4 };
+        System.out.println(minSwaps(arr));
+    }
 }
--- weekly-problems/src/class_2022_10_2/Code01_RangesHasDominateNumber.java
+++ weekly-problems/src/class_2022_10_2/Code01_RangesHasDominateNumber.java
@@ -1,4 +1,7 @@
 package class_2022_10_2;
+
+import java.util.HashMap;
+
 //来自小红书
 //小A认为如果在数组中有一个数出现了至少k次
 //且这个数是该数组的众数，即出现次数最多的数之一
@@ -8,4 +11,91 @@ package class_2022_10_2;
 //2 <= k <= n <= 100000
 //1 <= 数组的值 <= n
 public class Code01_RangesHasDominateNumber {
+
+    // 暴力方法
+    // 为了验证
+    // 时间复杂度O(N^3)
+    public static int dominates1(int[] arr, int k) {
+        int n = arr.length;
+        int ans = 0;
+        for (int l = 0; l < n; l++) {
+            for (int r = l; r < n; r++) {
+                if (ok(arr, l, r, k)) {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static boolean ok(int[] arr, int l, int r, int k) {
+        //键未数组原始的值,值为出现的次数
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = l; i <= r; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+        for (int times : map.values()) {
+            if (times >= k) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // 正式方法:滑动窗口+双指针(先求不被支配的区间数，再用总区间数减去它)
+    // 时间复杂度O(N)
+    public static int dominates2(int[] arr, int k) {
+        int n = arr.length;
+        // 总数量,即数组中所有可能的区间数
+        int all = n * (n + 1) / 2;
+        // 不被支配的区间数量
+        int except = 0;
+        // 次数表:当前窗口内数值v的出现次数
+        // 0 : 0
+        // 1 : 0
+        // 2 : 0
+        int[] cnt = new int[n + 1];
+        // 滑动窗口的左右指针,窗口为左闭右开
+        for (int l = 0, r = 0; l < n; l++) {
+            // [r] 即将要进来的
+            // cnt[arr[r]] + 1 < k
+            while (r < n && cnt[arr[r]] + 1 < k) {
+                // cnt[arr[r]]++;
+                // r++
+                cnt[arr[r++]]++;
+            }
+            // 累加以l为左端点的“不被支配区间”数量：窗口[l, r)内所有数的出现次数都<k，因此以l为左端点、右端点在[l, r-1]的区间都是不被支配的，数量为r-l
+            except += r - l;
+            //左指针收缩
+            cnt[arr[l]]--;
+        }
+        return all - except;
+    }
+
+    // 为了测试
+    public static int[] randomArray(int n) {
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            ans[i] = (int) (Math.random() * n) + 1;
+        }
+        return ans;
+    }
+
+    // 为了测试
+    public static void main(String[] args) {
+        int N = 100;
+        int testTimes = 5000;
+        System.out.println("测试开始");
+        for (int i = 0; i < testTimes; i++) {
+            int n = (int) (Math.random() * N) + 1;
+            int[] arr = randomArray(n);
+            int k = (int) (Math.random() * n) + 1;
+            int ans1 = dominates1(arr, k);
+            int ans2 = dominates2(arr, k);
+            if (ans1 != ans2) {
+                System.out.println("出错了!");
+            }
+        }
+        System.out.println("测试结束");
+    }
 }
